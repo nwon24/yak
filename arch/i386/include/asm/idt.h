7ad1b1c147e599219f791178f8dd66408e553ee4
nwon24 <WON0128@mhs.vic.edu.au>
2021-11-27 19:00:53 +1100

    arch: i386: idt.h: Interrupt Descriptor Table Header File

---
+++ arch/i386/include/asm/idt.h
@@ -0,0 +1,33 @@
+#ifndef _IDT_H
+#define _IDT_H
+
+#define IDT_32BIT_INT_GATE	0xE
+#define IDT_32BIT_TRAP_GATE	0xF
+
+#define IDT_ENTRY_PRESENT	0x80
+
+#define IDT_ENTRIES		256
+#define IDT_LIMIT		((IDT_ENTRIES << 3) - 1)
+
+#ifndef _ASSEMBLY_
+
+#include <stdint.h>
+
+#include <asm/paging.h>
+
+void set_idt_entry(int n, uint32_t offset, uint16_t selector, uint8_t dpl, uint8_t type);
+void idt_init(void);
+
+static inline void
+load_idt(void *idt_ptr)
+{
+	struct {
+		uint16_t limit;
+		uint32_t base;
+	}__attribute__((packed)) idt_desc = { IDT_LIMIT, (uint32_t)idt_ptr };
+	__asm__ volatile("lidt %0" : : "m" (idt_desc));
+}
+
+#endif /* _ASSEMBLY_ */
+
+#endif /* _IDT_H */
