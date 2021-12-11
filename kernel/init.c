342fb3d77ccaf1745a88056d7bd642858c18a2fd
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-11 14:54:04 +1100

    Init functions array definition and function to run them all

---
+++ kernel/init.c
@@ -0,0 +1,31 @@
+/*
+ * init.c
+ * Contains table of init functions.
+ */
+
+#include <stddef.h>
+
+#include <kernel/config.h>
+#include <kernel/init.h>
+
+#include <drivers/tty.h>
+
+#ifdef _CONFIG_DRIVER_8250_UART
+#include <drivers/8250_uart.h>
+#endif /* _CONFIG_DRIVER_8250_UART */
+
+init_function init_func_table[] = {
+#ifdef _CONFIG_DRIVER_8250_UART
+	uart8250_serial_init,
+#endif /* _CONFIG_DRIVER_8250_UART */
+	tty_init,
+	NULL,
+};
+
+void run_init_functions(void)
+{
+	init_function *func;
+
+	for (func = init_func_table; *func != NULL; func++)
+		(*func)();
+}
