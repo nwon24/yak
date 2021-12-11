0d0d5fccbd9f872207b963bc2c2271a5889b4594
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-11 14:20:27 +1100

    Remove inline assembly to enable and disable interrupts

--- arch/i386/include/asm/interrupts.h
+++ arch/i386/include/asm/interrupts.h
@@ -1,19 +1,20 @@
 #ifndef _INTERRUPTS_H
 #define _INTERRUPTS_H
 
-#define ENABLE_INTERRUPTS	__asm__("sti")
-#define DISABLE_INTERRUPTS	__asm__("cli")
 #ifndef _ASSEMBLY_
 
 #include <asm/8259_pic.h>
 #include <asm/idt.h>
+
+void enable_intr(void);
+void disable_intr(void);
 
 static inline void
 interrupts_init(void)
 {
 	idt_init();
 	pic_remap();
-	ENABLE_INTERRUPTS;
+	enable_intr();
 }
 
 #endif /* _ASSEMBLY_ */
