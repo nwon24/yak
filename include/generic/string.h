4663f6a008538dbbe1516e577e7bf19a134e51b9
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-11 14:21:58 +1100

    Added function prototype for strlen

--- include/generic/string.h
+++ include/generic/string.h
@@ -10,6 +10,8 @@ void *memmove(void *dst, const void *src, size_t count);
 int memcmp(const void *str1, const void *str2, size_t count);
 void *memset(void *bufp, int c, size_t count);
 
+size_t strlen(const char *str);
+
 #endif /* _ASSEMBLY_ */
 
 #endif /* _GENERIC_STRING_H */
