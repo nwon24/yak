9cb5166aac36ced77aece14b9712d449c3983ea7
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-11 14:22:13 +1100

    Added macros for drivers

--- include/kernel/config.h
+++ include/kernel/config.h
@@ -8,4 +8,11 @@
 
 #define KERNEL_VIRT_BASE	0xC0000000
 
+#define _CONFIG_ARCH_X86
+
+#define _CONFIG_DRIVER_8250_UART
+
+/* Should be enough for now, right? */
+#define _CONFIG_DRIVER_NR_DRIVERS	100
+
 #endif /* _CONFIG_H */
