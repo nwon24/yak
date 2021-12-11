2d174c922e3928243a940e3d1c735df3fb24e52a
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-11 14:20:41 +1100

    Remove inline assembly to do port I/O

--- arch/i386/include/asm/port_io.h
+++ arch/i386/include/asm/port_io.h
@@ -5,7 +5,7 @@
 
 #include <stdint.h>
 
-static inline void
+/* static inline void
 outb(uint8_t data, uint16_t port)
 {
 	__asm__ volatile("outb %0, %1" : : "a" (data), "Nd" (port));
@@ -18,7 +18,13 @@ inb(uint16_t port)
 
 	__asm__ volatile("inb %1, %0" : "=a" (ret) : "Nd" (port));
 	return (ret);
-}
+} */
+
+void port_outb(uint8_t data, uint16_t port);
+uint8_t port_inb(uint16_t port);
+
+#define outb	port_outb
+#define inb	port_inb
 
 #endif /* _ASSEMBLY_ */
 
