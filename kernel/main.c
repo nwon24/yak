2ca3cd447b162e73a181a50bcf828b54002ce450
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-11 14:22:41 +1100

    Change main to kernel_main and test printk

--- kernel/main.c
+++ kernel/main.c
@@ -5,9 +5,23 @@
 
 #include <asm/interrupts.h>
 
+#include <kernel/debug.h>
+#include <kernel/init.h>
+
+#include <drivers/8250_uart.h>
+
+/*
+ * Why isn't this called main()?
+ * Because at -O2 GCC puts this in .text.startup, which messes with the paging.
+ * Since the .text, .rodata, .data and .bss sections are mapped with different
+ * permissions, it is vital the mapping is correct and symbols are in the correct
+ * sections.
+ */
 void
-main(void)
+kernel_main(void)
 {
 	interrupts_init();
+	run_init_functions();
+	printk("Hello, world!\r\n");
 	while (1);
 }
