7afed01e719db12ed50433a45191b8f130d472aa
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-12 11:27:16 +1100

    mm header files

---
+++ include/mm/mm.h
@@ -0,0 +1,8 @@
+#ifndef _MM_H
+#define _MM_H
+
+#include <generic/multiboot.h>
+
+void mm_init(multiboot_info_t *mb_info);
+
+#endif /* _MM_H */
---
+++ include/mm/mmap.h
@@ -0,0 +1,26 @@
+#ifndef _MMAP_H
+#define _MMAP_H
+
+#include <stdint.h>
+
+#define MAX_MMAP_ENTRIES	16
+
+struct mmap_entry {
+	uint32_t base;			/* Base address of memory */
+	uint32_t size;			/* Size of region */
+	uint32_t bitmap_size;		/* Size of bitmap, in bytes */
+	struct mmap_entry *next;	/* Next entry */
+};
+
+struct mmap_bitmap {
+	struct mmap_entry *mmap_entry;
+	uint32_t base;
+	uint32_t size;
+};
+
+extern struct mmap_entry mmap[];
+extern struct mmap_bitmap mmap_bitmaps[];
+
+extern uint32_t total_mem;
+
+#endif /* _MMAP_H */
