7e00921b7a3e753e2248f4f39c4d1e7f9c6ad0c3
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-13 22:49:03 +1100

    Added functon to map linear framebuffer

--- arch/i386/virtual_mm.c
+++ arch/i386/virtual_mm.c
@@ -12,6 +12,7 @@
 #include <mm/mm.h>
 #include <mm/mmap.h>
 
+/* Virtual address of temporary page table */
 #define VIRT_ADDR_TMP_PAGE	0xC03FF000
 
 extern uint32_t init_page_directory[];
@@ -21,12 +22,18 @@ static uint32_t *current_pg_table = NULL;
 
 
 static inline void
-tmp_map_page(uint32_t *page)
+tmp_map_page(uint32_t page)
 {
 	uint32_t virt;
 
 	virt = VIRT_ADDR_TMP_PAGE;
-	init_page_table[(virt >> VIRT_ADDR_PG_TAB_SHIFT) & VIRT_ADDR_PG_TAB_MASK] = (uint32_t)page | PAGE_PRESENT | PAGE_WRITABLE;
+	init_page_table[(virt >> VIRT_ADDR_PG_TAB_SHIFT) & VIRT_ADDR_PG_TAB_MASK] = page | PAGE_PRESENT | PAGE_WRITABLE;
+}
+
+static inline uint32_t
+get_tmp_page(void)
+{
+	return init_page_table[(VIRT_ADDR_TMP_PAGE >> VIRT_ADDR_PG_TAB_SHIFT) & VIRT_ADDR_PG_TAB_MASK] & ~VIRT_ADDR_FRAME_MASK;
 }
 
 /*
@@ -50,7 +57,8 @@ kernel_virt_map_page(uint32_t page)
 		p = (uint32_t *)page_frame_alloc();
 		if ((uint32_t)p == NO_FREE_PAGE)
 			panic("Out of physical memory");
-		tmp_map_page(p);
+		tlb_flush(get_tmp_page());
+		tmp_map_page((uint32_t)p);
 		tmp = (uint32_t *)(char *)init_page_directory + (KERNEL_VIRT_BASE >> VIRT_ADDR_PG_DIR_SHIFT);
 		while ((*tmp & PAGE_PRESENT) && tmp < init_page_directory + (PAGE_SIZE / sizeof(*p) - 1))
 			tmp++;
@@ -62,4 +70,34 @@ kernel_virt_map_page(uint32_t page)
 	}
 	*p = (uint32_t)page | PAGE_WRITABLE | PAGE_PRESENT;
 	return KERNEL_VIRT_BASE | (p - current_pg_table) << VIRT_ADDR_PG_TAB_SHIFT;
+}
+
+/*
+ * A special function called to map video memory.
+ * It maps a 4 MiB block of memory into the upper 4 MiB of the virtual address space
+ * The argument it takes is the physical address of the framebuffer.
+ * Tricky thing is to make sure we preserve the temporary mapping of the current page table.
+ * /
+ */
+uint32_t
+virt_map_fb(uint32_t fb)
+{
+	uint32_t old, fb_pg_table, *p, i;
+	int pg_dir_entry;
+
+	old = get_tmp_page();
+	printk("old %x\r\n", old);
+	if ((fb_pg_table = page_frame_alloc()) == NO_FREE_PAGE)
+		return 0;
+	tmp_map_page(fb_pg_table);
+	tlb_flush(VIRT_ADDR_TMP_PAGE);
+	p = (uint32_t *)VIRT_ADDR_TMP_PAGE;
+	for (i = fb ; p < (uint32_t *)VIRT_ADDR_TMP_PAGE + (PAGE_SIZE / sizeof(*p)); p++, i += PAGE_SIZE)
+		*p = i | PAGE_PRESENT | PAGE_WRITABLE;
+	tmp_map_page(old);
+	tlb_flush(VIRT_ADDR_TMP_PAGE);
+	/* Map it to the last page table entry (i.e., last 4 MiB of address space) */
+	pg_dir_entry = PAGE_SIZE / sizeof(*p) - 1;
+	init_page_directory[pg_dir_entry] = fb_pg_table | PAGE_PRESENT | PAGE_WRITABLE;
+	return pg_dir_entry << VIRT_ADDR_PG_DIR_SHIFT;
 }
