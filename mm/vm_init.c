78540b80429b7fb9a7ea117d67b800be21f8eecd
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-13 15:13:49 +1100

    Initialise kernel heap

--- mm/vm_init.c
+++ mm/vm_init.c
@@ -1,20 +1,33 @@
 /*
  * vm_init.c
  * Initialise virtual memory manager.
+ * Shouldn't really be in its own file...
  */
 
 #include <asm/paging.h>
 
+#include <kernel/debug.h>
+
 #include <mm/mm.h>
 #include <mm/vm.h>
 
-#define INIT_HEAP_SIZE	0x400000
+static uint32_t kernel_heap_start;
 
 void
 vm_init(void)
 {
 	int i;
 
-	for (i = 0; i < INIT_HEAP_SIZE / PAGE_SIZE; i++)
-		kernel_virt_put_page();
+	/*
+	 * We coun't on there being at least 4 MiB free memory.
+	 * Even QEMU has 128 MiB free memory by default.
+	 */
+	for (i = 0; i < HEAP_SIZE / PAGE_SIZE; i++) {
+		if (!i)
+			kernel_heap_start = kernel_virt_put_page();
+		else
+			kernel_virt_put_page();
+	}
+	printk("Kernel heap begins at %x\r\n", kernel_heap_start);
+	kvmalloc_init(kernel_heap_start, HEAP_SIZE);
 }
