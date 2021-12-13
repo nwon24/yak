9980338f5c3e759ed4c017d7990977a4a3f11723
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-13 15:13:05 +1100

    Updated function prototypes

--- include/mm/vm.h
+++ include/mm/vm.h
@@ -1,6 +1,25 @@
 #ifndef _VM_H
 #define _VM_H
 
+#include <stdint.h>
+#include <stddef.h>
+
+#define HEAP_SIZE	0x400000
+
+/*
+ * Header of a chunk of virtual memory.
+ * 'base' is the virtual address.
+ */
+struct vm_chunk_header {
+	uint32_t base;
+	uint32_t size;
+	struct vm_chunk_header *next;
+	struct vm_chunk_header *prev;
+};
+
 void vm_init(void);
+void kvmalloc_init(uint32_t heap, uint32_t size);
+void *kvmalloc(size_t bytes);
+void kvmfree(void *p);
 
 #endif /* _VM_H */
