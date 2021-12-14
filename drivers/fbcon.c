801ef308ae3399d2681f9b160a5d7db267be34b9
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-14 14:43:20 +1100

    Added interface to virtual console driver

--- drivers/fbcon.c
+++ drivers/fbcon.c
@@ -9,15 +9,18 @@
 #include <drivers/fb.h>
 #include <drivers/fbcon.h>
 #include <drivers/vbe.h>
+#include <drivers/virtual_console.h>
 
 #include <kernel/debug.h>
 
 static struct fbcon fb_console;
 static struct fbcon *current_fbcon;
 
-static int fbcon_putc(int c);
+static int fbcon_putc(struct virtual_console *vc, int c);
+static int fbcon_puts(struct virtual_console *vc, char *s, int len);
 static void fbcon_putc_32bpp(struct bitmap_font *font, unsigned char *data, uint32_t pitch, uint32_t bg, uint32_t fg, uint32_t dst);
 static void fbcon_putc_16bpp(struct bitmap_font *font, unsigned char *data, uint32_t pitch, uint16_t bg, uint16_t fg, uint32_t dst);
+static void fbcon_get_dimensions(uint32_t *width, uint32_t *height);
 
 static uint32_t ansi_colors_fg_32rgb[] = {
         0x000000,	/* Black */
@@ -69,15 +72,21 @@ static uint16_t ansi_colors_fg_16rgb[] = {
         0xD69A,
 };
 
+static struct virtual_console_driver vc_fbcon_driver = {
+        .vc_putc = fbcon_putc,
+        .vc_puts = fbcon_puts,
+        .vc_get_dimensions = fbcon_get_dimensions,
+};
+
 void
 fbcon_init(struct fb_info *info)
 {
+        int i;
+
         fb_console.fbcon_info = info;
         printk("bpp %d, height %d width %d\r\n", info->mode_info.bpp, info->mode_info.height, info->mode_info.width);
         if ((fb_console.fbcon_font = load_default_font()) == NULL)
                 panic("Unable to load bitmap font");
-        fb_console.fbcon_cx = 0;
-        fb_console.fbcon_cy = 0;
         if (info->mode_info.bpp == 32) {
                 fb_console.fbcon_fg = ansi_colors_fg_32rgb[ANSI_FG_DEFAULT - ANSI_FG_COLOR_BASE];
                 fb_console.fbcon_bg = ansi_colors_bg_32rgb[ANSI_BG_DEFAULT - ANSI_BG_COLOR_BASE];
@@ -87,22 +96,24 @@ fbcon_init(struct fb_info *info)
         } else {
                 panic("fbcon_init: Unrecognised bits per pixel");
         }
+        for (i = 0; i < NR_VIRTUAL_CONSOLES; i++)
+                register_vc_driver(i, &vc_fbcon_driver);
         current_fbcon = &fb_console;
 }
 
-int
-fbcon_puts(char *s, int len)
+static int
+fbcon_puts(struct virtual_console *vc, char *s, int len)
 {
         char *p;
 
         p = s;
         while (len-- && *s)
-                fbcon_putc(*s++);
+                fbcon_putc(vc, *s++);
         return p - s;
 }
 
 static int
-fbcon_putc(int c)
+fbcon_putc(struct virtual_console *vc, int c)
 {
         unsigned char *data;
         struct bitmap_font *font;
@@ -116,14 +127,13 @@ fbcon_putc(int c)
         if (c < 0 || (uint32_t)c >= font->nr_glpyh)
                 c = 0;
         data = font->data + c * font->bytes_per_glpyh;
-        off = (current_fbcon->fbcon_cy * font->height * mode_info->pitch)
-              + (current_fbcon->fbcon_cx * (font->width + 1) * sizeof(uint32_t));
+        off = (vc->vc_cy * font->height * mode_info->pitch)
+              + (vc->vc_cx * (font->width + 1) * sizeof(uint32_t));
         dst = info->fb_virt_addr + off;
         if (mode_info->bpp == 32)
                 fbcon_putc_32bpp(font, data, mode_info->pitch, current_fbcon->fbcon_bg, current_fbcon->fbcon_fg, dst);
         else if (mode_info->bpp == 16)
                 fbcon_putc_16bpp(font, data, mode_info->pitch, current_fbcon->fbcon_bg, current_fbcon->fbcon_fg, dst);
-        current_fbcon->fbcon_cx++;
         return c;
 }
 
@@ -161,4 +171,14 @@ fbcon_putc_16bpp(struct bitmap_font *font, unsigned char *data, uint32_t pitch, 
                data += bytes_per_line;
                dst += pitch - font->width * sizeof(uint16_t);
         }
+}
+
+static void
+fbcon_get_dimensions(uint32_t *width, uint32_t *height)
+{
+        struct fb_info *info;
+
+        info = fb_console.fbcon_info;
+        *width = info->mode_info.width / fb_console.fbcon_font->width;
+        *height = info->mode_info.height / fb_console.fbcon_font->height;
 }
