abbe80ba92b85fce554a5a3d720a679bb74b23cf
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-14 22:01:54 +1100

    Added second buffer to virtual console struct and changed parameters to driver interface

--- include/drivers/virtual_console.h
+++ include/drivers/virtual_console.h
@@ -10,29 +10,31 @@
  * Structure of a virtual console.
  * @vc_cx: Current virtual x coordinate after a write.
  * @vc_cy: Current virtual y coordinate after a wrie
- * @vc_old_cx: Value of x coorindate before write
- * @vc_old_cy: Value of y coordinate before write
  * After vc_flush(), all the above variables should be the same.
  * @vc_width: Width of console in characters (fbcon works this out)
  * @vc_height: Same, but for height
- * @vc_scr_buf: Internal buffer of characters written to the screen.
+ * @vc_scr_buf: Internal buffer of characters to be written to the screen.
+ * @vc_cur_buf: Internal buffer of characters currently being displayed.
  * This is used because it makes it easier to do scrolling, clearing, etc.
  * @vc_scr_size: Size of internal buffer in bytes.
+ * *vc_saved_* is for the ESC 7/8 escape sequence.
  */
 struct virtual_console {
         uint32_t vc_cx;
         uint32_t vc_cy;
-        uint32_t vc_old_cx;
-        uint32_t vc_old_cy;
+
+        uint32_t vc_saved_cx;
+        uint32_t vc_saved_cy;
+
         uint32_t vc_width;
         uint32_t vc_height;
         char *vc_scr_buf;
+        char *vc_cur_buf;
         uint32_t vc_scr_size;
 };
 
 struct virtual_console_driver {
-        int (*vc_putc)(struct virtual_console *vc, int c);
-        int (*vc_puts)(struct virtual_console *vc, char *s, int len);
+        int (*vc_putc)(uint32_t cx, uint32_t cy, int c);
         void (*vc_get_dimensions)(uint32_t *width, uint32_t *height);
 };
 
