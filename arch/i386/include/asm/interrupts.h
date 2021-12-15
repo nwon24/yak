6428a28c9fefe8538cf69849280b525be2caeb54
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-15 18:02:03 +1100

    Added macros for enable/disable interrupts

--- arch/i386/include/asm/interrupts.h
+++ arch/i386/include/asm/interrupts.h
@@ -6,8 +6,15 @@
 #include <asm/8259_pic.h>
 #include <asm/idt.h>
 
+#include <kernel/config.h>
+
+#ifdef _CONFIG_USE_INLINE_ASM
+#define enable_intr()	__asm__("sti")
+#define disable_intr()	__asm__("cli")
+#else
 void enable_intr(void);
 void disable_intr(void);
+#endif /* _CONFIG_USE_INLINE_ASM */
 
 static inline void
 interrupts_init(void)
