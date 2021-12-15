c30d7b21d90b36fd185e975f1424932bc3b274f6
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-15 14:15:16 +1100

    Use __arch_* versions of string functions
    Use arch-specific inline versions of mem* functions if possible

--- include/generic/string.h
+++ include/generic/string.h
@@ -5,10 +5,20 @@
 
 #include <stddef.h>
 
+#include <kernel/config.h>
+
+#ifdef _CONFIG_USE_ARCH_STRING_H
+#include <asm/arch_string.h>
+#define memcpy	__arch_memcpy
+#define memmove __arch_memmove
+#define memcmp __arch_memcmp
+#define memset __arch_memset
+#else
 void *memcpy(void *dst, const void *src, size_t count);
 void *memmove(void *dst, const void *src, size_t count);
 int memcmp(const void *str1, const void *str2, size_t count);
 void *memset(void *bufp, int c, size_t count);
+#endif /* _CONFIG_ARCH_USE_STRING_H */
 
 size_t strlen(const char *str);
 
