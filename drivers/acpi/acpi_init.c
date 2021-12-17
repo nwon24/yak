048937b983ebaece333dd05e5c77e11e1350a131
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-17 14:17:55 +1100

    Find RSDT.

--- drivers/acpi/acpi_init.c
+++ drivers/acpi/acpi_init.c
@@ -14,6 +14,8 @@
 
 #define EBDA_ADDR	(*(uint16_t *)VIRT_ADDR(0x40E))
 
+int acpi_supported = 0;
+
 struct rsdp_desc {
         char signature[8];
         uint8_t checksum;
@@ -22,7 +24,25 @@ struct rsdp_desc {
         uint32_t rsdt_addr;
 }__attribute__((packed));
 
+struct acpi_sdt_header {
+        char signature[4];
+        uint32_t length;
+        uint8_t revision;
+        uint8_t checksum;
+        char oemid[6];
+        char oem_table_id[8];
+        uint32_t oem_revision;
+        uint32_t creator_id;
+        uint32_t creator_revision;
+}__attribute__((packed));
+
+struct rsdt {
+        struct acpi_sdt_header header;
+        uint32_t *sdt_pointers;
+};
+
 static struct rsdp_desc *find_rsdp(void);
+static int acpi_table_check(struct acpi_sdt_header *header);
 
 static struct rsdp_desc *
 find_rsdp(void)
@@ -41,15 +61,36 @@ find_rsdp(void)
         }
         return NULL;
 }
+
+static int
+acpi_table_check(struct acpi_sdt_header *header)
+{
+        unsigned char sum = 0;
+        uint32_t i;
+
+        for (i = 0; i < header->length; i++)
+                sum += *((char *)header + i);
+        return sum == 0;
+}
+
+int
+is_acpi_supported(void)
+{
+        return acpi_supported;
+}
+
 void
 acpi_init(void)
 {
         struct rsdp_desc *rsdp;
+        struct rsdt *rsdt;
         int sum;
         char *p;
 
-        if ((rsdp = find_rsdp()) == NULL)
-                printk("ACPI not supported.\r\n");
+        if ((rsdp = find_rsdp()) == NULL) {
+                printk("ACPI not supported\r\n");
+                return;
+        }
         /*
          * Make sure the table is valid.
          * Casing the sum of all the struct's members to a byte
@@ -57,6 +98,14 @@ acpi_init(void)
          */
         for (p = (char *)rsdp, sum = 0; p < (char *)rsdp +sizeof(*rsdp); p++)
                 sum += *p;
-        if ((uint8_t)sum)
+        if ((uint8_t)sum) {
                 printk("Invalid RSDP\r\n");
+                return;
+        }
+        rsdt = (struct rsdt *)(virt_map_phys(rsdp->rsdt_addr & 0xFFFFF000) + (rsdp->rsdt_addr & VIRT_ADDR_FRAME_MASK));
+        if (acpi_table_check(&rsdt->header) == 0) {
+                printk("Invaid RSDT\r\n");
+                return;
+        }
+        acpi_supported = 1;
 }
