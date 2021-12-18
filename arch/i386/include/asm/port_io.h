c19e33bdf6e65374ca3caf7b08c0c356d199dc0c
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-18 22:26:34 +1100

    Added port I/O functions for 16 and 32 bit ports

--- arch/i386/include/asm/port_io.h
+++ arch/i386/include/asm/port_io.h
@@ -22,12 +22,54 @@ inb(uint16_t port)
 	return ret;
 }
 
-#else
+static inline void
+outw(uint16_t data, uint16_t port)
+{
+	__asm__ volatile("outw %0, %1" : : "a" (data), "Nd" (port));
+}
+
+static inline uint16_t
+inw(uint16_t port)
+{
+	uint16_t ret;
+
+	__asm__ volatile("inw %1, %0" : "=a" (ret) : "Nd" (port));
+	return ret;
+}
+
+static inline void
+outl(uint32_t data, uint32_t port)
+{
+	__asm__ volatile("outl %0, %1" : : "a" (data), "Nd" (port));
+}
+
+static inline uint32_t
+inl(uint32_t port)
+{
+	uint32_t ret;
+
+	__asm__ volatile("inl %1, %0" : "=a" (ret) : "Nd" (port));
+	return ret;
+}
+#else /* _CONFIG_USE_INLINE_ASM */
+
 void port_outb(uint8_t data, uint16_t port);
 uint8_t port_inb(uint16_t port);
 
+void port_outw(uint16_t data, uint16_t port);
+uint16_t port_inw(uint16_t port);
+
+void port_outl(uint32_t data, uint32_t port);
+uint32_t port_inl(uint32_t port);
+
 #define outb	port_outb
 #define inb	port_inb
+
+#define outw	port_outw
+#define inw	port_inw
+
+#define outl	port_outl
+#define inl	port_inl
 
 #endif /* _CONFIG_USE_INLINE_ASM */
 #endif /* _ASSEMBLY_ */
