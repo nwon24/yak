2f58de61a3917af00b77e92a19ca5fcb2483f9fd
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-18 22:42:08 +1100

    Added a few function prototypes

--- include/drivers/acpi.h
+++ include/drivers/acpi.h
@@ -1,6 +1,15 @@
 #ifndef _ACPI_H_
 #define _ACPI_H_
 
+#include <stdint.h>
+
+enum acpi_version {
+	ACPI_1_0,
+	ACPI_2_0,
+};
+
 void acpi_init(void);
+int is_acpi_supported(void);
+uint16_t get_acpi_boot_arch_flags(void);
 
 #endif /* ACPI_H_ */
