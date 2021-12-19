884439b804a610d7961a8aa4a2709bdde897352a
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-20 10:29:43 +1100

    Add more warnings to GCC flags.

--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ CC := $(TARGET_TRIPLET)-gcc
 AR := $(TARGET_TRIPLET)-ar
 CPP := $(CC) -E
 
-CFLAGS :=  -std=c99 -ffreestanding -O2 -Wall -Wextra -pedantic -Wshadow -Wstrict-prototypes -MMD -MP -D_KERNEL_
+CFLAGS :=  -std=c99 -ffreestanding -O2 -Wall -Wextra -Werror -Winit-self -Wundef -pedantic -Wshadow -Wstrict-prototypes -MMD -MP -D_KERNEL_
 CPPFLAGS := -ffreestanding -P
 # Still C compiler flags as we assemble .S files using GCC
 ASFLAGS := -ffreestanding -D_ASSEMBLY_ -MMD -MP
