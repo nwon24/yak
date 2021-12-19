2850c945f71540d06302aec6928aa373ca3662f5
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-20 10:31:28 +1100

    Programmable Interval Timer Driver

---
+++ drivers/pit.c
@@ -0,0 +1,64 @@
+/*
+ * pit.c
+ * Programmable interrupt timer driver.
+ */
+#include <kernel/config.h>
+
+#ifdef _CONFIG_ARCH_X86
+#include <stdint.h>
+
+#include <asm/8259_pic.h>
+#include <asm/port_io.h>
+#include <asm/idt.h>
+#include <asm/irq.h>
+#include <asm/segment.h>
+
+#include <drivers/driver.h>
+#include <drivers/timer.h>
+
+#include <kernel/debug.h>
+
+#define PIT_CHAN0_DATA	0x40
+#define PIT_CHAN1_DATA	0x41
+#define PIT_CHAN2_DATA	0x42
+#define PIT_MODE_CMD	0x43
+
+#define PIT_SQUARE_WAVE_MODE	0x36
+
+#define PIT_IRQ		0x20
+
+#define FREQUENCY	1193182
+
+static void pit_irq_handler(void);
+int pit_init(void);
+
+static struct timer_driver pit_interface = {
+	.init = pit_init,
+	.irq_handler = pit_irq_handler
+};
+
+static inline void
+pit_set_reload(uint16_t reload)
+{
+	outb(reload & 0xFF, PIT_CHAN0_DATA);
+	outb((reload >> 8) & 0xFF, PIT_CHAN0_DATA);
+}
+
+int
+pit_init(void)
+{
+	register_timer_driver(&pit_interface);
+	set_idt_entry(PIT_IRQ, (uint32_t)irq0_handler, KERNEL_CS_SELECTOR, DPL_0, IDT_32BIT_INT_GATE);
+	outb(PIT_SQUARE_WAVE_MODE, PIT_MODE_CMD);
+	pit_set_reload(FREQUENCY / HZ);
+	pic_clear_mask(PIT_IRQ);
+	return 0;
+}
+
+static void
+pit_irq_handler(void)
+{
+	/* Nothing */
+}
+
+#endif /* _CONFIG_ARCH_X86 */
