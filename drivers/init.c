669f52fa3ea3e8aaf0965a57530f9785833538fe
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-20 10:30:41 +1100

    Added 'timer_init' and 'pit_init' to init functions.
    This device initialisation interface isn't really that great.

--- drivers/init.c
+++ drivers/init.c
@@ -10,11 +10,16 @@
 #include <drivers/init.h>
 #include <drivers/tty.h>
 #include <drivers/fb.h>
+#include <drivers/timer.h>
 #include <drivers/virtual_console.h>
 
 #ifdef _CONFIG_DRIVER_8250_UART
 #include <drivers/8250_uart.h>
 #endif /* _CONFIG_DRIVER_8250_UART */
+
+#ifdef _CONFIG_ARCH_X86
+int pit_init(void);
+#endif /* _CONFIG_ARCH_X86 */
 
 typedef int (*init_function)(void);
 
@@ -23,6 +28,9 @@ init_function critical_init_func_table[] = {
 #ifdef _CONFIG_DRIVER_8250_UART
 	uart8250_serial_init,
 #endif
+#ifdef _CONFIG_ARCH_X86
+	pit_init,
+#endif /* _CONFIG_ARCH_X86 */
 	NULL
 };
 
@@ -31,6 +39,7 @@ init_function init_func_table[] = {
 	fb_init,
 #endif /* _CONFIG_DRIVER_FBDEV */
 	vc_init,
+	timer_init,
 	NULL,
 };
 
