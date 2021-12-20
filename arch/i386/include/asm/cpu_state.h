52eb67734bec9453c1a902e53d00f0f1e62f874c
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-20 17:57:51 +1100

    Header file for the cpu state.

---
+++ arch/i386/include/asm/cpu_state.h
@@ -0,0 +1,30 @@
+#ifndef _CPU_STATE_H
+#define _CPU_STATE_H
+
+#include <stdint.h>
+
+struct i386_cpu_state {
+	uint32_t ebp;
+	uint32_t edi;
+	uint32_t esi;
+	uint32_t edx;
+	uint32_t ecx;
+	uint32_t ebx;
+	uint32_t eax;
+
+	uint32_t gs;
+	uint32_t fs;
+	uint32_t es;
+	uint32_t ds;
+
+	uint32_t eip;
+	uint32_t cs;
+	uint32_t eflags;
+	uint32_t esp;
+	uint32_t ss;
+};
+
+void cpu_state_init(void);
+struct i386_cpu_state *cpu_state_save(struct i386_cpu_state *new);
+
+#endif /* _CPU_STATE_H */
