377649ab283d36ff5166f992119fc86ccdd38b0a
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-23 14:51:22 +1100

    Updated cpu_state struct to have room for an error (dummy and real)

--- arch/i386/include/asm/cpu_state.h
+++ arch/i386/include/asm/cpu_state.h
@@ -17,14 +17,21 @@ struct i386_cpu_state {
 	uint32_t es;
 	uint32_t ds;
 
+	uint32_t error;
+
 	uint32_t eip;
 	uint32_t cs;
 	uint32_t eflags;
 	uint32_t esp;
 	uint32_t ss;
+
+	uint32_t cr3;
+	uint32_t kernel_stack;
 };
 
 void cpu_state_init(void);
 struct i386_cpu_state *cpu_state_save(struct i386_cpu_state *new);
+
+extern struct i386_cpu_state *current_cpu_state;
 
 #endif /* _CPU_STATE_H */
