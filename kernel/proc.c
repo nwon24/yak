c81d34a3f22fead7270c82a3bee4ec071ea62c9c
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-23 14:56:29 +1100

    Added a function to initialise management of processes

--- kernel/proc.c
+++ kernel/proc.c
@@ -2,6 +2,25 @@
  * proc.c
  * Handles processes.
  */
+#include <kernel/debug.h>
 #include <kernel/proc.h>
 
-struct process process_table[NR_PROC];
+static struct process process_table[NR_PROC];
+struct process *current_process;
+
+extern uint32_t _start_user_head, _end_user_head;
+
+int arch_processes_init(uint32_t start, uint32_t size);
+
+void
+processes_init(void)
+{
+	struct process *proc = FIRST_PROC;
+
+	current_process = proc;
+	proc->pid = 0;
+	proc->state = PROC_RUNNABLE;
+	proc->tty = 0;
+	if (arch_processes_init((uint32_t)&_start_user_head, &_end_user_head - &_start_user_head) < 0)
+		panic("Unable to initialise processes");;
+}
