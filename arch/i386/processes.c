caa20108c5752a46c4bd6557206afe49ff2f718c
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-24 09:59:58 +1100

    Began implementation of 'fork' system call

--- arch/i386/processes.c
+++ arch/i386/processes.c
@@ -6,6 +6,9 @@
 #include <asm/paging.h>
 #include <asm/user_mode.h>
 
+#include <generic/errno.h>
+#include <generic/string.h>
+
 #include <kernel/debug.h>
 #include <kernel/proc.h>
 
@@ -33,4 +36,24 @@ arch_processes_init(uint32_t start, uint32_t size)
 	 */
 	move_to_user(0, 4096);
 	return 0;
+}
+
+/*
+ * Architecture specifics of fork routine.
+ * Integer passed is PID of child process.
+ */
+int
+arch_fork(int child)
+{
+	struct i386_cpu_state *new;
+
+	if ((new = cpu_states + child) >= cpu_states + NR_PROC)
+		return -EAGAIN;
+	memmove(new, current_cpu_state, sizeof(*new));
+	/* Return value for child process is 0 */
+	new->eax = 0;
+	if ((new->cr3 = page_frame_alloc()) == NO_FREE_PAGE)
+		return -ENOMEM;
+	copy_address_space(current_cpu_state->cr3, new->cr3);
+	return child;
 }
