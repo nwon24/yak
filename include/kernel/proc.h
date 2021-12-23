cf3331ed1b436dabb5fb0a1a6fc4f16881054035
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-23 14:55:18 +1100

    Added function prototypes and macros

--- include/kernel/proc.h
+++ include/kernel/proc.h
@@ -11,4 +11,13 @@ struct process {
 
 extern struct process *current_process;
 
+void processes_init(void);
+
+#define FIRST_PROC	(&process_table[0])
+#define LAST_PROC	(&process_table[NR_PROC])
+
+#define PROC_RUNNING	1
+#define PROC_BLOCKED	2
+#define PROC_RUNNABLE	3
+
 #endif /* _PROC_H */
