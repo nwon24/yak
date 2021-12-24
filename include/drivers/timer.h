8e5e829eb6ba5d21a9b98c05038d9005ec7671aa
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-24 16:02:54 +1100

    Add external declarations of global variables.
    Variables are 'timer_ticks' and 'starting_time', which tell how many
    timer interrupts have occured and the starting time of the system in
    seconds since Jan 1 1970, respectively.

--- include/drivers/timer.h
+++ include/drivers/timer.h
@@ -1,6 +1,8 @@
 #ifndef _TIMER_H
 #define _TIMER_H
 
+#include <stdint.h>
+
 /* 100 times per second = 10ms */
 #define HZ	100
 
@@ -11,5 +13,10 @@ struct timer_driver {
 
 int timer_init(void);
 struct timer_driver *register_timer_driver(struct timer_driver *driver);
+
+extern uint32_t timer_ticks;
+extern uint32_t starting_time;
+
+#define CURRENT_TIME	(starting_time + timer_ticks / HZ)
 
 #endif /* _TIMER_H */
