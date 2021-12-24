feb53cf39e1b046188e9a55c6e016e1e0c0580c9
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-24 11:10:47 +1100

    Syscall header file

---
+++ arch/i386/include/asm/syscall.h
@@ -0,0 +1,25 @@
+#ifndef _SYSCALL_H
+#define _SYSCALL_H
+
+#include <stdint.h>
+
+/* Should be enough for now */
+#define NR_SYSCALL 100
+
+#define SYSCALL_IRQ	0x80
+
+struct syscall_entry {
+	uint32_t addr;
+	uint32_t nr_args;
+};
+
+extern struct syscall_entry syscall_table[];
+
+static inline void
+register_syscall(int n, uint32_t addr, uint32_t nr_args)
+{
+	syscall_table[n].addr = addr;
+	syscall_table[n].nr_args = nr_args;
+}
+ 
+#endif /* _SYSCALL_H */
