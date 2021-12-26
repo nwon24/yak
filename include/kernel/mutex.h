66462e77994af73a33702f8a39ab6767540df6ee
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-26 22:39:53 +1100

    Mutex header file

---
+++ include/kernel/mutex.h
@@ -0,0 +1,15 @@
+#ifndef _MUTEX_H
+#define _MUTEX_H
+
+#define MUTEX_UNLOCKED	0
+#define MUTEX_LOCKED	1
+
+struct process;
+
+typedef int mutex;
+
+void mutex_init(mutex *mutex);
+void mutex_lock(mutex *mutex);
+void mutex_unlock(mutex *mutex);
+
+#endif /* _MUTEX_H */
