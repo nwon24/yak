93d5e4efe71464f92b9cb85f2272733c884ce0a6
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-30 10:02:34 +1100

    Added a bunch of interface functions

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -28,46 +28,60 @@ static void pci_check_bus(uint8_t bus);
 static void pci_check_buses(void);
 static void pci_check_device(uint8_t bus, uint8_t device);
 static void pci_check_function(uint8_t bus, uint8_t device, uint8_t function);
-static uint8_t pci_get_secondary_bus(uint8_t bus, uint8_t device, uint8_t function);
+static uint8_t get_secondary_bus(uint8_t bus, uint8_t device, uint8_t function);
 
 static struct pci_entry pci_table[PCI_MAX_BUSES * PCI_MAX_DEVICES * PCI_MAX_FUNCTIONS];
 static struct pci_entry *next_entry = NULL;
 static int pci_entries = 0;
 
 static inline uint16_t
-pci_get_vendor_id(uint8_t bus, uint8_t device, uint8_t function)
+get_vendor_id(uint8_t bus, uint8_t device, uint8_t function)
 {
 	return pci_read_register(bus, device, function, 0) & 0xFFFF;
 }
 
 static inline uint16_t
-pci_get_device_id(uint8_t bus, uint8_t device, uint8_t function)
+get_device_id(uint8_t bus, uint8_t device, uint8_t function)
 {
 	return (pci_read_register(bus, device, function, 0) >> 16) & 0xFFFF;
 }
 
 static inline uint8_t
-pci_get_header_type(uint8_t bus, uint8_t device, uint8_t function)
+get_header_type(uint8_t bus, uint8_t device, uint8_t function)
 {
 	return (pci_read_register(bus, device, function, 0xC) >> 16) & 0xFF;
 }
 
 static inline uint8_t
-pci_get_class_code(uint8_t bus, uint8_t device, uint8_t function)
+get_class_code(uint8_t bus, uint8_t device, uint8_t function)
 {
 	return pci_read_register(bus, device, function, 0x8) >> 24;
 }
 
 static inline uint8_t
-pci_get_subclass(uint8_t bus, uint8_t device, uint8_t function)
+get_subclass(uint8_t bus, uint8_t device, uint8_t function)
 {
 	return (pci_read_register(bus, device, function, 0x8) >> 16) & 0xFF;
 }
 
 static inline uint8_t
-pci_get_secondary_bus(uint8_t bus, uint8_t device, uint8_t function)
+get_secondary_bus(uint8_t bus, uint8_t device, uint8_t function)
 {
 	return (pci_read_register(bus, device, function, 0x18) >> 8) & 0xFF;
+}
+
+static inline uint8_t
+get_prog_if(uint8_t bus, uint8_t device, uint8_t function)
+{
+	return (pci_read_register(bus, device, function, 0x8) >> 8) & 0xFF;
+}
+
+static inline uint32_t
+get_bar(uint8_t bus, uint8_t device, uint8_t function, int bar)
+{
+	if (bar > 5 || bar < 0)
+		return 0;
+	return pci_read_register(bus, device, function, 0x10 + (bar << 2);
 }
 
 /*
@@ -82,6 +96,12 @@ pci_init(void)
 	pci_enumerate();
 }
 
+int
+pci_nr_entries(void)
+{
+	return pci_entries;
+}
+
 static void
 pci_enumerate(void)
 {
@@ -94,15 +114,15 @@ pci_check_function(uint8_t bus, uint8_t device, uint8_t function)
 	uint8_t class_code, subclass, secondary_bus;
 	uint16_t vendor_id;
 
-	class_code = pci_get_class_code(bus, device, function);
-	subclass = pci_get_subclass(bus, device, function);
+	class_code = get_class_code(bus, device, function);
+	subclass = get_subclass(bus, device, function);
 	/* 0x4 subclass means PCI-to-PCI bridge. */
 	if ((class_code == PCI_CLASS_CODE_BRIDGE) && (subclass == 0x4)) {
-		secondary_bus = pci_get_secondary_bus(bus, device, function);
+		secondary_bus = get_secondary_bus(bus, device, function);
 		pci_check_bus(secondary_bus);
 		return;
 	}
-	if ((vendor_id = pci_get_vendor_id(bus, device, function)) != PCI_FUNCTION_NON_EXISTENT) {
+	if ((vendor_id = get_vendor_id(bus, device, function)) != PCI_FUNCTION_NON_EXISTENT) {
 		next_entry->bus = bus;
 		next_entry->device = device;
 		next_entry->function = function;
@@ -138,10 +158,10 @@ pci_check_device(uint8_t bus, uint8_t device)
 	uint8_t func = 0;
 	uint8_t header_type;
 
-	if (pci_get_vendor_id(bus, device, func) == PCI_DEVICE_NON_EXISTENT)
+	if (get_vendor_id(bus, device, func) == PCI_DEVICE_NON_EXISTENT)
 		return;
 	pci_check_function(bus, device, func);
-	header_type = pci_get_header_type(bus, device, func);
+	header_type = get_header_type(bus, device, func);
 	if (header_type & PCI_DEVICE_MULTIPLE_FUNC) {
 		for (func = 1; func < PCI_MAX_FUNCTIONS; func++)
 			pci_check_function(bus, device, func);
@@ -153,13 +173,79 @@ pci_check_buses(void)
 {
 	uint8_t func;
 
-	if (!(pci_get_header_type(0, 0, 0) & PCI_DEVICE_MULTIPLE_FUNC)) {
+	if (!(get_header_type(0, 0, 0) & PCI_DEVICE_MULTIPLE_FUNC)) {
 		pci_check_bus(0);
 	} else {
 		for (func = 0; func < PCI_MAX_FUNCTIONS; func++) {
-			if (pci_get_vendor_id(0, 0, func) != PCI_FUNCTION_NON_EXISTENT)
+			if (get_vendor_id(0, 0, func) != PCI_FUNCTION_NON_EXISTENT)
 				break;
 			pci_check_bus(func);
 		}
 	}
+}
+
+/*
+ * Interface used by drivers.
+ * Just thin wrappers.
+ * Function names have 'pci_' added to the front.
+ */
+
+int
+pci_find_class_code(int code, uint8_t *bus, uint8_t *device, uint8_t *function)
+{
+	struct pci_entry *p;
+	int ret = 0;
+
+	for (p = pci_table; p < pci_table + pci_entries; p++) {
+		if (get_class_code(p->bus, p->device, p->function) == code) {
+			ret = 1;
+			*bus = p->bus;
+			*device = p->device;
+			*function = p->function;
+			break;
+		}
+	}
+	return ret;
+}
+
+uint16_t
+pci_get_vendor_id(uint8_t bus, uint8_t device, uint8_t function)
+{
+	return get_vendor_id(bus, device, function);
+}
+
+uint16_t
+pci_get_device_id(uint8_t bus, uint8_t device, uint8_t function)
+{
+	return get_device_id(bus, device, function);
+}
+
+uint8_t
+pci_get_header_type(uint8_t bus, uint8_t device, uint8_t function)
+{
+	return get_header_type(bus, device, function);
+}
+
+uint8_t
+pci_get_class_code(uint8_t bus, uint8_t device, uint8_t function)
+{
+	return get_class_code(bus, device, function);
+}
+
+uint8_t
+pci_get_subclass(uint8_t bus, uint8_t device, uint8_t function)
+{
+	return get_subclass(bus, device, function);
+}
+
+uint8_t
+pci_get_prog_if(uint8_t bus, uint8_t device, uint8_t function)
+{
+	return get_prog_if(bus, device, function);
+}
+
+uint32_t
+pci_get_bar(uint8_t bus, uint8_t device, uint8_t function, int bar)
+{
+	return get_bar(bus, device, function, bar);
 }
