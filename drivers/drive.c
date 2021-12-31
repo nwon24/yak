ce4d0a480b1239f7ad75839a78f2de4178264340
nwon24 <WON0128@mhs.vic.edu.au>
2021-12-31 22:56:20 +1100

    Device independent disk driver

---
+++ drivers/drive.c
@@ -0,0 +1,72 @@
+/*
+ * drive.c
+ * Contains the device independent disk driver.
+ */
+
+#include <drivers/ata.h>
+#include <drivers/pci.h>
+
+#include <kernel/debug.h>
+
+enum msd_subclass {
+	SCSI_BUS_CTRL,		/* SCSI Bus Controller */
+	IDE_CTRL,		/* IDE controller */
+	FLOPPY_DISK_CTRL,	/* Floppy disk controller */
+	IPI_BUS_CTRL,		/* IPI Bus Controller (??) */
+	RAID_CTRL,		/* RAID controller */
+	ATA_CTRL,		/* ATA Controller with ADMA interface */
+	SATA_CTRL,		/* Serial ATA Controler */
+	SCSI_CTRL,		/* Serial Attached SCSI Controller */
+	NON_VOLATILE_MEM_CTRL,	/* Non-volatile memory controller */
+	OTHER = 0x80		/* Vendor specific */
+};
+
+int
+drive_init(void)
+{
+	uint8_t bus, dev, func, prog_if; 
+	uint32_t bar0, bar1, bar2, bar3, bar4;
+	int pri_irq, sec_irq;
+	enum ata_port_width width1, width2;
+
+	if (!pci_find_class_code(PCI_CLASS_CODE_MASS_STORAGE, &bus, &dev, &func))
+		panic("No mass storage device found.");
+	if (pci_get_subclass(bus, dev, func) != IDE_CTRL)
+		panic("Currently do not have drivers for anything other than IDE controllers.");
+	pri_irq = ATA_PRI_IRQ;
+	sec_irq = ATA_SEC_IRQ;
+	prog_if = pci_get_prog_if(bus, dev, func);
+	if (prog_if & (1 << 0)) {
+		/* Primary channel is in PCI native mode */
+		bar0 = pci_get_bar(bus, dev, func, 0);
+		bar0 &= 0xFFFFFFFC;
+		bar1 = pci_get_bar(bus, dev, func, 1) + 2;
+		bar1 &= 0xFFFFFFFC;
+		width1 = ATA_PORT_32;
+		pri_irq = sec_irq = pci_get_int_line(bus, dev, func);
+	} else {
+		bar0 = ATA_PRI_CMD_BASE;
+		bar1 = ATA_PRI_CTRL_BASE;
+		width1 = ATA_PORT_16;
+	}
+	if (prog_if & (1 << 2)) {
+		/* Secondary channel is in PCI native mode */
+		bar2 = pci_get_bar(bus, dev, func, 2);
+		bar2 &= 0xFFFFFFFC;
+		bar3 = pci_get_bar(bus, dev, func, 3) + 2;
+		bar3 &= 0xFFFFFFFC;
+		width2 = ATA_PORT_32;
+		pri_irq = sec_irq = pci_get_int_line(bus, dev, func);
+	} else {
+		bar2 = ATA_SEC_CMD_BASE;
+		bar3 = ATA_SEC_CTRL_BASE;
+		width2 = ATA_PORT_16;
+	}
+	if (prog_if & (1 << 7))
+		/* Bus master IDE controller */
+		bar4 = pci_get_bar(bus, dev, func, 4);
+	else
+		bar4 = 0;
+	ata_probe(bar0, bar1, bar2, bar3, bar4, pri_irq, sec_irq, width1, width2);
+	return 0;
+}
---
+++ include/drivers/drive.h
@@ -0,0 +1,6 @@
+#ifndef DRIVE_H
+#define DRIVE_H
+
+int drive_init(void);
+
+#endif /* DRIVE_H */
