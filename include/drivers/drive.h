65661dba1945492e1c3a206a6ea53bfd5b88564b
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-01 21:21:33 +1100

    Added driver struct to interface with device dependent driver

--- include/drivers/drive.h
+++ include/drivers/drive.h
@@ -1,6 +1,18 @@
+/*
+ * Didn't feel write to call it hd_* something
+ * as SSDs are not disks...
+ */
 #ifndef DRIVE_H
 #define DRIVE_H
 
+#include <stddef.h>
+
+struct drive_driver {
+	int (*drive_start)(int drive, char *buf, size_t count, size_t lba, int rw);
+	void (*drive_intr)(void);
+};
+
 int drive_init(void);
+void drive_driver_register(struct drive_driver *drv);
 
 #endif /* DRIVE_H */
