cf285bb58928d498604abf55c0f59c0e4b8509ce
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-01 14:53:13 +1100

    Code to handle ATA requests.

---
+++ drivers/ata/ata_request.c
@@ -0,0 +1,113 @@
+/*
+ * ata_request.c
+ * Handles requests to the ATA controller.
+ */
+#include <stddef.h>
+
+#include <kernel/config.h>
+#include <kernel/debug.h>
+#include <kernel/proc.h>
+
+#include <drivers/ata.h>
+
+static void ata_start_request_lba28(struct ata_request *req);
+static void ata_start_request_lba48(struct ata_request *req);
+
+struct ata_request ata_requests[ATA_MAX_REQUESTS];
+
+struct ata_request *ata_current_req = NULL;
+
+#ifdef CONFIG_ARCH_X86
+struct ata_request *
+ata_build_request(struct ata_device *dev, uint32_t lba, int count, int rw, char *buf)
+{
+	struct ata_request *req;
+
+	for (req = ata_requests; req < ata_requests + ATA_MAX_REQUESTS; req++) {
+		if (req->dev == NULL)
+			break;
+	}
+	if (req >= ata_requests + ATA_MAX_REQUESTS)
+		return NULL;
+	req->dev = dev;
+	req->lba = lba;
+	req->count = count;
+	req->rw = rw;
+	req->buf = buf;
+	req->next = NULL;
+	return req;
+}
+#endif /* CONFIG_ARCH_X86 */
+
+void
+ata_add_request(struct ata_request *req)
+{
+	struct ata_request *tmp;
+
+	if (ata_current_req == NULL) {
+		ata_current_req = req;
+		ata_start_request(req);
+	} else {
+		for (tmp = ata_current_req; tmp->next != NULL; tmp = tmp->next);
+		tmp->next = req;
+	}
+}
+
+void
+ata_finish_request(struct ata_request *req)
+{
+	req->dev = NULL;
+}
+
+void
+ata_start_request(struct ata_request *req)
+{
+	if (req->lba <= 0x0FFFFFFF)
+		ata_start_request_lba28(req);
+	else
+		ata_start_request_lba48(req);
+}
+
+
+/*
+ * With these next two routines, we are assuming the caller knows what they are doing,
+ * i.e., 48-bit LBA requires the ATA_CMD_READ_SECTORS_EXT and ATA_CMD_WRITE_SECTORS_EXT commands.
+ */
+static void
+ata_start_request_lba28(struct ata_request *req)
+{
+	struct ata_device *dev = req->dev;
+	/*
+	 * Cannot use 'ata_select_drive' since the upper 4 bits are of the LBA.
+	 */
+	ata_reg_write(dev, 0xE0 | (dev->drive << 4) | ((req->lba >> 24) & 0x0F), ATA_REG_DRIVE);
+	ata_reg_write(dev, (unsigned char)req->count, ATA_REG_SEC_COUNT);
+	ata_reg_write(dev, req->lba & 0xFF, ATA_REG_LBA1);
+	ata_reg_write(dev, (req->lba >> 8) & 0xFF, ATA_REG_LBA2);
+	ata_reg_write(dev, (req->lba >> 16) & 0xFF, ATA_REG_LBA3);
+	ata_reg_write(dev, req->cmd, ATA_REG_CMD);
+}
+
+#ifdef CONFIG_ARCH_X86
+static void
+ata_start_request_lba48(struct ata_request *req)
+{
+	struct ata_device *dev = req->dev;
+
+	ata_reg_write(dev, 0xE0 | (dev->drive << 4) | ((req->lba >> 24) & 0x0F), ATA_REG_DRIVE);
+	ata_reg_write(dev, (req->count >> 8) & 0xFF, ATA_REG_SEC_COUNT);
+	/*
+	 * Numbering the 48-bit LBA value from low to high, bytes 5 and 6 are zero if we are on
+	 * a 32-bit system. This is why we have the '#ifdef..." above. If we try shifting a 32-bit value
+	 * more than 32-bits to the left to get the upper 16 bits, the compiler won't be happy.
+	 */
+	ata_reg_write(dev, (req->lba >> 24) & 0xFF, ATA_REG_LBA1);
+	ata_reg_write(dev, 0, ATA_REG_LBA2);
+	ata_reg_write(dev, 0, ATA_REG_LBA3);
+	ata_reg_write(dev, req->count & 0xFF, ATA_REG_SEC_COUNT);
+	ata_reg_write(dev, req->lba & 0xFF, ATA_REG_LBA1);
+	ata_reg_write(dev, (req->lba >> 8) & 0xFF, ATA_REG_LBA2);
+	ata_reg_write(dev, (req->lba >> 16) & 0xFF, ATA_REG_LBA3);
+	ata_reg_write(dev, req->cmd, ATA_REG_CMD);
+}
+#endif /* CONFIG_ARCH_X86 */
