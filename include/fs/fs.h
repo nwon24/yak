29f3fbe54e2ef873ca1ec85846f1f9b51534f308
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-04 22:34:44 +1100

    Main filesystem header file.

---
+++ include/fs/fs.h
@@ -0,0 +1,50 @@
+#ifndef FS_H
+#define FS_H
+
+#include <stddef.h>
+
+#include <asm/types.h>
+
+#include <kernel/mutex.h>
+
+#define DEFAULT_BLOCK_SIZE	1024
+
+#define NR_MOUNTS	10
+
+enum {
+	READ,
+	WRITE
+};
+
+enum filesystem {
+	EXT2
+	/* We don't support any other filesystems... */
+};
+
+enum fs_attribute_cmd {
+	GET_BLOCKSIZE,
+	GET_MTIME,
+	GET_WTIME,
+};
+
+struct generic_filesystem;
+/*
+ * Will add to this.
+ */
+struct fs_driver_ops {
+	size_t (*fs_get_attribute)(struct generic_filesystem *fs, enum fs_attribute_cmd cmd);
+};
+
+struct generic_filesystem {
+	enum filesystem f_fs;
+	void *f_super;
+	dev_t f_dev;
+	mutex f_mutex;
+	struct fs_driver_ops *f_driver;
+};
+
+struct generic_filesystem *find_filesystem(dev_t dev);
+struct generic_filesystem *register_filesystem(struct generic_filesystem *fs);
+size_t filesystem_get_attr(dev_t dev, enum fs_attribute_cmd cmd);
+
+#endif /* FS_H */
