1dbd7748e59fcea1cc66ac46d813e44e90efba19
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-04 16:35:54 +1100

    POSIX types header for x86

---
+++ arch/i386/include/asm/types.h
@@ -0,0 +1,23 @@
+#ifndef ASM_TYPES_H
+#define ASM_TYPES_H
+
+#include <stdint.h>
+
+typedef uint32_t dev_t;
+typedef int32_t ssize_t;
+typedef int32_t blksize_t;
+typedef int32_t pid_t;
+typedef uint32_t time_t;
+typedef uint32_t clock_t;
+typedef int32_t blkcnt_t;
+typedef int32_t off_t;
+typedef int mode_t;
+typedef int nlink_t;
+typedef int uid_t;
+typedef int gid_t;
+typedef int id_t;
+typedef uint32_t fsblkcnt_t;
+typedef uint32_t fsfilcnt_t;
+typedef uint32_t ino_t;
+
+#endif /* ASM_TYPE_H */
