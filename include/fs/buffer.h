c182392b9322b91d0f38ae8deea2504d0a4b5a81
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-04 16:35:38 +1100

    Header file for buffer cache.

---
+++ include/fs/buffer.h
@@ -0,0 +1,39 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include <asm/types.h>
+
+#include <kernel/config.h>
+#include <kernel/mutex.h>
+
+#define NR_BUF_HASH	CONFIG_FS_BUF_HASH_SIZE
+#define NR_BUF_BUFFERS	CONFIG_FS_BUF_CACHE_SIZE
+
+/*
+ * The objects of the file system's buffer
+ * cache.
+ */
+struct buffer {
+	dev_t b_dev;
+	size_t b_blknr;
+	blksize_t b_blksize;
+	char *b_data;
+	mutex b_mutex;
+	int b_flags;
+
+	struct buffer *b_next_free;
+	struct buffer *b_prev_free;
+	struct buffer *b_next;
+	struct buffer *b_prev;
+};
+
+#define B_DWRITE	(1 << 0)	/* Delayed write */
+#define B_DONE		(1 << 1)	/* Successful completion */
+#define B_ERROR		(1 << 2)	/* Error */
+
+void buffer_init(void);
+
+#endif /* BUFFER_H */
