f690a45e8d71ae22e2cf77f0e2835e7dbdf5a71c
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-04 12:05:18 +1100

    Include '<drivers/mbr.h>'

--- include/drivers/drive.h
+++ include/drivers/drive.h
@@ -7,10 +7,26 @@
 
 #include <stddef.h>
 
+#include <kernel/config.h>
+
+#ifdef CONFIG_DISK_MBR
+#include <drivers/mbr.h>
+#else
+#error "We don't support GPT yet."
+#endif /* CONFIG_DISK_MBR */
+
 #define SECTOR_SIZE	512
+
+struct generic_disk_dev {
+	struct drive_driver *driver;
+	int minor;
+	size_t lba_start;
+	size_t lba_len;
+};
 
 struct drive_driver {
 	int (*drive_start)(int drive, char *buf, size_t count, size_t lba, int rw);
+	int (*drive_exists)(int dev);
 	void (*drive_intr)(void);
 };
 
