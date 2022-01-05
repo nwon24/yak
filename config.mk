1998a709b55c27b5962605eb0771355fcd4de649
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-05 11:33:20 +1100

    Added toolchain options for using clang

--- config.mk
+++ config.mk
@@ -1,16 +1,29 @@
 TARGET_TRIPLET := i686-elf
 TARGET_ARCH := i386
 
+TOOLCHAIN ?= GNU
+
+ifeq ($(TOOLCHAIN), GNU)
 CC := $(TARGET_TRIPLET)-gcc
 LD := $(CC)
 AR := $(TARGET_TRIPLET)-ar
+else ifeq ($(TOOLCHAIN), LLVM)
+CC := clang -target $(TARGET_TRIPLET)
+LD := ld.lld
+AR := llvm-ar
+else
+$(error No toolchain specified. Use 'make TOOLCHAIN={GNU, LLVM}')
+endif
 CPP := $(CC) -E
 
 CFLAGS :=  -std=c99 -ffreestanding -O2 -Wall -Wextra -Werror -Winit-self -Wundef -pedantic -Wshadow -Wstrict-prototypes -MMD -MP -DKERNEL
 CPPFLAGS := -ffreestanding -P
 # Still C compiler flags as we assemble .S files using GCC
 ASFLAGS := -ffreestanding -MMD -MP
-LDFLAGS := -nostdlib -O2 -lgcc
+LDFLAGS := -nostdlib -O2
+ifeq ($(CC), $(TARGET_TRIPLET)-gcc)
+LDFLAGS += -lgcc
+endif
 
 ARCH_DIR := arch/$(TARGET_ARCH)
 
