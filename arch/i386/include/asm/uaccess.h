35d003f61af31c1aeecc471ce8b79aa73ed8aad9
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-07 20:33:01 +1100

    User space access and architecture depdenent unistd.h headers.

---
+++ arch/i386/include/asm/uaccess.h
@@ -0,0 +1,21 @@
+#ifndef UACCESS_H
+#define UACCESS_H
+
+#include <stdint.h>
+
+/*
+ * With the way paging is set up for x86,
+ * user space data can be accessed without any special
+ * function. However, hide it in a macro in case this
+ * ever changes. We always just need to check the pointer
+ * is okay and mapped.
+ */
+#define get_ubyte(ptr)	(*(uin8_t *)(ptr))
+#define get_uword(ptr)	(*(uint16_t *)(ptr))
+#define get_ulong(ptr)	(*(uint32_t *)(ptr))
+
+#define put_ubyte(ptr, b)	(*(uint8_t *)(ptr) = b)
+#define put_uword(ptr, w)	(*(uint16_t *)(ptr) = w)
+#define put_ulong(ptr, l)	(*(uint32_t *)(ptr) = l)
+
+#endif /* UACCESS_H */
---
+++ arch/i386/include/asm/unistd.h
@@ -0,0 +1,7 @@
+#ifndef ARCH_UNISTD_H
+#define ARCH_UNISTD_H
+
+#define __NR_fork	0x2
+#define __NR_open	0x5
+
+#endif /* ARCH_UNISTD_H */
