b0c8c91763df07ddbc3429a46a480373cb3b3f9b
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-08 21:29:31 +1100

    Begun implementation of 'open' for ext2.

--- fs/open.c
+++ fs/open.c
@@ -2,6 +2,7 @@
  * open.c
  * The 'open' system call.
  */
+#include <asm/uaccess.h>
 #include <asm/paging.h>
 #include <asm/types.h>
 
@@ -11,6 +12,8 @@
 
 #include <kernel/debug.h>
 #include <kernel/proc.h>
+
+static struct generic_filesystem *get_fs_from_path(const char *path);
 
 struct file file_table[NR_FILE];
 
@@ -38,19 +41,43 @@ find_fd(void)
 	return -1;
 }
 
+/*
+ * From a path, find the filesystem it is mounted under.
+ * Returns NULL if invalid.
+ * Pointer should already have been verified.
+ */
+static struct generic_filesystem *
+get_fs_from_path(const char *path)
+{
+	if (path == NULL)
+		return NULL;
+	/*
+	 * TODO; Do not support mounts yet.
+	 * Just assume it is the root filesystem.
+	 */
+	return mount_table[0];
+}
+
 int
 kernel_open(const char *path, int flags, mode_t mode)
 {
 	struct file *fp;
-	int fd;
+	struct generic_filesystem *fs;
+	int fd, err;
 
 	if (path == NULL || !check_user_ptr((void *)path))
+		return -EINVAL;
+	if ((fs = get_fs_from_path(path)) == NULL)
 		return -EINVAL;
 	if ((fd = find_fd()) < 0)
 		return -ENFILE;
 	if ((fp = get_free_file()) == NULL)
 		return -ENFILE;
-	printk("here\r\n");
+	if ((fs = get_fs_from_path(path)) == NULL)
+		return -EINVAL;
+	fp->f_inode = fs->f_driver->fs_open(path, flags, mode, &err);
+	if (fp->f_inode == NULL)
+		return err;
 	current_process->file_table[fd] = fp;
 	fp->f_pos = 0;
 	fp->f_mode = mode;
