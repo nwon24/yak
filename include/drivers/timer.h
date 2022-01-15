2408dd2d27172ec63b77c6d66176c88f39f30ed1
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-15 22:10:50 +1100

    Fixed wrong variable name - 'startup_time' instead of 'starting_time'

--- include/drivers/timer.h
+++ include/drivers/timer.h
@@ -15,8 +15,8 @@ int timer_init(void);
 struct timer_driver *register_timer_driver(struct timer_driver *driver);
 
 extern uint32_t timer_ticks;
-extern uint32_t starting_time;
+extern uint32_t startup_time;
 
-#define CURRENT_TIME	(starting_time + timer_ticks / HZ)
+#define CURRENT_TIME	(startup_time + timer_ticks / HZ)
 
 #endif /* TIMER_H */
