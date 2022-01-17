c4f12b2b7ed50f1fae4147850cc73fea719cfb04
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-18 08:32:40 +1100

    Made header file assembly friendly.

--- include/generic/fcntl.h
+++ include/generic/fcntl.h
@@ -1,8 +1,15 @@
 #ifndef FCNTL_H
 #define FCNTL_H
 
+#ifndef __ASSEMBLER__
+
 #include <asm/types.h>
 
+int open(const char *path, int flags, ...);
+
+#endif /* __ASSEMBLER__ */
+
+#define O_ACCMODE	03	/* Lower 2 bits of mode */
 #define O_RDONLY	00
 #define O_WRONLY	01
 #define O_RDWR		02
@@ -11,7 +18,5 @@
 #define O_NOCTTY	00400
 #define O_TRUNC		01000
 #define O_APPEND	02000
-
-int open(const char *path, int flags, ...);
 
 #endif /* FCNTL_H */
