36729543e8c5fc08159ab97c198be3a8f78b99a0
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-17 14:08:24 +1100

    Added a bunch of checks to 'open' routine.

--- fs/ext2/open.c
+++ fs/ext2/open.c
@@ -5,9 +5,13 @@
 #include <fs/fs.h>
 #include <fs/ext2.h>
 
+#include <fs/dev.h>
+
 #include <generic/errno.h>
+#include <generic/fcntl.h>
 
 #include <kernel/debug.h>
+#include <kernel/proc.h>
 
 /*
  * Pointer should already have been verified.
@@ -16,16 +20,31 @@ void *
 ext2_open(const char *path, int flags, int mode, int *err)
 {
 	struct ext2_inode_m *ip;
-	int rand;
 
 	if ((ip = ext2_namei(path, err)) == NULL) {
 		printk("not found err ret %p %d\r\n", ip, *err);
 		return NULL;
 	}
+	mode &= ~current_process->umask & 0777;
+	if (((flags & O_RDONLY) || (flags & O_RDWR)) && ext2_permission(ip, PERM_READ) < 0) {
+		*err = -EACCES;
+		return NULL;
+	}
+	if (((flags & O_WRONLY) || (flags & O_RDWR)) && ext2_permission(ip, PERM_WRITE) < 0) {
+		*err = -EACCES;
+		return NULL;
+	}
 
-	rand = flags;
-	rand = mode;
-	rand++;
+	if (((flags & O_RDWR) || (flags & O_WRONLY)) && EXT2_S_ISDIR(ip->i_ino.i_mode)) {
+		*err = -EISDIR;
+		return NULL;
+	}
+
+	if ((flags & O_TRUNC) && EXT2_S_ISREG(ip->i_ino.i_mode))
+		ext2_itrunc(ip);
+
+	if (EXT2_S_ISCHR(ip->i_ino.i_mode))
+		chr_dev_open(ip->i_ino.i_block[0]);
 	printk("ext2_open: ip->i_mode %x\r\n", ip->i_ino.i_mode);
 	return ip;
 }
