9213f1f25da3e4f16b6b9a4206ea7705c3e3213d
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-18 09:58:41 +1100

    Added 'sync' system call

--- fs/sys1.c
+++ fs/sys1.c
@@ -6,6 +6,7 @@
 #include <asm/paging.h>
 #include <asm/types.h>
 
+#include <fs/buffer.h>
 #include <fs/fs.h>
 
 #include <generic/errno.h>
@@ -116,4 +117,18 @@ kernel_write(int fd, void *buf, size_t count)
 	if (count == 0)
 		return 0;
 	return fp->f_fs->f_driver->fs_write(fp, buf, count);
+}
+
+void
+kernel_sync(void)
+{
+	struct generic_filesystem **fs;
+
+	for (fs = mount_table; fs < mount_table + NR_MOUNTS; fs++) {
+		if (*fs == NULL)
+			continue;
+		if ((*fs)->f_driver->fs_sync(*fs) < 0)
+			printk("WARNING: Sync for device %x returned error.\r\n", (*fs)->f_dev);
+	}
+	buffer_sync();
 }
