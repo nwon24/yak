83f463ab876842831ad23fac01932c7bb37fabdd
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-20 22:52:41 +1100

    Added 'SEEK_*' constants.
    Hopeflly the actual values don't matter.

--- arch/i386/include/asm/unistd.h
+++ arch/i386/include/asm/unistd.h
@@ -1,11 +1,16 @@
 #ifndef ARCH_UNISTD_H
 #define ARCH_UNISTD_H
 
+#define SEEK_SET	0
+#define SEEK_CUR	1
+#define SEEK_END	2
+
 #define __NR_fork	0x2
 #define __NR_read	0x3
 #define __NR_write	0x4
 #define __NR_open	0x5
 #define __NR_unlink	0xA
+#define __NR_lseek	0x13
 #define __NR_sync	0x24
 
 #endif /* ARCH_UNISTD_H */
