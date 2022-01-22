750b354f9e627e8ab01784f7c278769d521b9b1b
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-22 15:04:37 +1100

    Added 'ext2_mknod' and 'ext2_close'

--- fs/ext2/open.c
+++ fs/ext2/open.c
@@ -3,6 +3,8 @@
  * Handles the 'open' syscall for an ext2 file.
  * Also has a some other related syscalls.
  */
+#include <asm/uaccess.h>
+
 #include <fs/fs.h>
 #include <fs/ext2.h>
 
@@ -26,33 +28,52 @@ void *
 ext2_open(const char *path, int flags, int mode, int *err)
 {
 	struct ext2_inode_m *ip, *base_dir;
-	const char *base;
+	const char *base, *p;
+	void *ret;
 
 	if ((ip = ext2_namei(path, err, &base, &base_dir, NULL)) == NULL) {
 		if (!(flags & O_CREAT)) {
+			ext2_iput(base_dir);
 			*err = -ENOENT;
-			return NULL;
-		}
-		if (!EXT2_S_ISDIR(base_dir->i_ino.i_mode))
-			return NULL;
-		if ((ip = ext2_new_file(base, base_dir, mode, 0, err)) != NULL)
+			ret = NULL;
 			goto end;
-		else
-			return NULL;
+		}
+		if (!EXT2_S_ISDIR(base_dir->i_ino.i_mode)) {
+			*err = -ENOTDIR;
+			ret = NULL;
+			goto end;
+		}
+		for (p = base; get_ubyte(p) != '\0'; p++) {
+			if (get_ubyte(p) == '/') {
+				*err = -ENOENT;
+				ret = NULL;
+				goto end;
+			}
+		}
+		if ((ip = ext2_new_file(base, base_dir, mode, 0, err)) != NULL) {
+			ret = ip;
+			goto end;
+		} else {
+			ret = NULL;
+			goto end;
+		}
 	}
 	mode &= ~current_process->umask & 0777;
 	if (!((flags & O_ACCMODE) == O_WRONLY) && ext2_permission(ip, PERM_READ) < 0) {
 		*err = -EACCES;
-		return NULL;
+		ret = NULL;
+		goto end;
 	}
 	if (!((flags & O_ACCMODE) == O_RDONLY) && ext2_permission(ip, PERM_WRITE) < 0) {
 		*err = -EACCES;
-		return NULL;
+		ret = NULL;
+		goto end;
 	}
 
 	if (!((flags & O_ACCMODE) == O_RDONLY) && EXT2_S_ISDIR(ip->i_ino.i_mode)) {
 		*err = -EISDIR;
-		return NULL;
+		ret = NULL;
+		goto end;
 	}
 
 	if ((flags & O_TRUNC) && EXT2_S_ISREG(ip->i_ino.i_mode))
@@ -60,10 +81,13 @@ ext2_open(const char *path, int flags, int mode, int *err)
 
 	if (EXT2_S_ISCHR(ip->i_ino.i_mode))
 		chr_dev_open(ip->i_ino.i_block[0]);
+	ret = ip;
  end:
 	printk("ext2_open: mode %x ip->i_mode %x num %d %p\r\n", mode, ip->i_ino.i_mode, ip->i_num, ip);
+	if (base_dir != ip)
+		ext2_iput(base_dir);
 	mutex_unlock(&ip->i_mutex);
-	return ip;
+	return ret;
 }
 
 /*
@@ -75,6 +99,7 @@ ext2_new_file(const char *name, struct ext2_inode_m *dir, mode_t mode, dev_t dev
 {
 	ino_t new;
 	struct ext2_inode_m *ip;
+	struct ext2_superblock_m *sb = get_ext2_superblock(dir->i_dev);
 
 	new = ext2_ialloc(dir->i_dev);
 	if (new == 0) {
@@ -86,6 +111,7 @@ ext2_new_file(const char *name, struct ext2_inode_m *dir, mode_t mode, dev_t dev
 		*err = -EIO;
 		return NULL;
 	}
+	memset(&ip->i_ino, 0, EXT2_INODE_SIZE(sb));
 	ip->i_ino.i_mode = mode;
 	ip->i_ino.i_uid = current_process->euid;
 	ip->i_ino.i_gid = current_process->egid;
@@ -103,6 +129,67 @@ ext2_new_file(const char *name, struct ext2_inode_m *dir, mode_t mode, dev_t dev
 		ext2_iput(ip);
 		return NULL;
 	}
-	ext2_iput(ip);
 	return ip;
+}
+
+int
+ext2_mknod(const char *path, mode_t mode, dev_t dev)
+{
+	struct ext2_inode_m *dp, *ip;
+	const char *base, *p;
+	int err;
+
+	mode &= ~current_process->umask;
+	ip = ext2_namei(path, &err, &base, &dp, NULL);
+	/*
+	 * We need to check no entry was found.
+	 * If it didn't find a directory, thatn -ENOENT should be returned.
+	 * If it didn't find the final componenet in the path, then that is good, since
+	 * that is what we are trying to make.
+	 */
+	if (ip != NULL) {
+		ext2_iput(ip);
+		if (ip != dp)
+			ext2_iput(dp);
+		return -EEXIST;
+	}
+	/*
+	 * If it failed for some other reason, just return that reason.
+	 */
+	if (err != -ENOENT) {
+		ext2_iput(dp);
+		return err;
+	}
+	/*
+	 * So now, we know that 'ext2_namei' failed by not being able to find an entry.
+	 * Just need to determine where it was a directory componenet or the final component (which shouldn't exist).
+	 */
+	p = base;
+	while (get_ubyte(p) != '\0' && get_ubyte(p) != '/')
+		p++;
+	if (get_ubyte(p) == '/') {
+		ext2_iput(dp);
+		return -ENOENT;
+	}
+	if ((ip = ext2_new_file(base, dp, mode, dev, &err)) == NULL) {
+		ext2_iput(dp);
+		return err;
+	}
+	ext2_iput(ip);
+	ext2_iput(dp);
+	return 0;
+}
+
+/*
+ * Reference count has already been dealt with, as that is not filesystem
+ * dependent.
+ */
+int
+ext2_close(struct file *file)
+{
+	if (file == NULL || file->f_inode == NULL)
+		panic("ext2_close: file == NULL or file->f_inode == NULL");
+	if (file->f_count == 0)
+		ext2_iput(file->f_inode);
+	return 0;
 }
