bc3e0cdc4b5134bd1561551dd6374910eb1d8324
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-26 09:55:12 +1100

    Added '__NR_waitpid'

--- arch/i386/include/asm/unistd.h
+++ arch/i386/include/asm/unistd.h
@@ -5,12 +5,13 @@
 #define SEEK_CUR	1
 #define SEEK_END	2
 
-#define __NR_exit	0x0
+#define __NR_exit	0x1
 #define __NR_fork	0x2
 #define __NR_read	0x3
 #define __NR_write	0x4
 #define __NR_open	0x5
 #define __NR_close	0x6
+#define __NR_waitpid	0x7
 #define __NR_creat	0x8
 #define __NR_link	0x9
 #define __NR_unlink	0xA
