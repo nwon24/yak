6e0b1d7ab64ac6fefe5de50475b080f2de07546c
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-25 14:31:51 +1100

    Refactored the mutex stuff

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -13,22 +13,41 @@
 void
 mutex_init(mutex *m)
 {
-	*m = MUTEX_UNLOCKED;
+	m->lock = MUTEX_UNLOCKED;
+	m->owner = NULL;
 }
 
 void
 mutex_lock(mutex *m)
 {
+	if (m->owner == NULL && m->lock == MUTEX_LOCKED) {
+		printk("mutex %p is corrupt: it has no owner but it is locked\r\n");
+		panic("mutex_lock: Corrupt mutex\r\n");
+	}
 	disable_intr();
-	while (*m == MUTEX_LOCKED)
-		sleep(m, PROC_SLEEP_INTERRUPTIBLE);
-	*m = MUTEX_LOCKED;
-	restore_intr_state();
+	if (m->owner != current_process) {
+		while (m->lock == MUTEX_LOCKED)
+			sleep(m, PROC_SLEEP_INTERRUPTIBLE);
+	}
+	m->lock = MUTEX_LOCKED;
+	m->owner = current_process;
+	/*	restore_intr_state(); */
+	enable_intr();
 }
 
 void
 mutex_unlock(mutex *m)
 {
-	*m = MUTEX_UNLOCKED;
+	disable_intr();
+	m->lock = MUTEX_UNLOCKED;
+	m->owner = NULL;
 	wakeup(m, WAKEUP_SWITCH);
+	/* restore_intr_state(); */
+	enable_intr();
+}
+
+int
+mutex_locked(mutex *m)
+{
+	return m->lock == MUTEX_LOCKED;
 }
