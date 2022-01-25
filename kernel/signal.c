2426a432389cd8508aec83efcc997afb6a1447d1
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-25 16:13:17 +1100

    Added signal table

--- kernel/signal.c
+++ kernel/signal.c
@@ -7,45 +7,47 @@
 
 #include <kernel/debug.h>
 
+static struct signal signal_table[NSIG];
+
 static inline void
-signal_init(struct process *proc, int signal, enum sig_def_action action)
+signal_init(int signal, enum sig_def_action action)
 {
 	if (signal == 0)
 		return;
-	proc->signals[signal - 1].def_action = action;
-	proc->signals[signal - 1].sig = signal;
+	signal_table[signal - 1].def_action = action;
+	signal_table[signal - 1].sig = signal;
 }
 
 void
-signals_init(struct process *proc)
+signals_init(void)
 {
-	signal_init(proc, SIGBUS, SIGACTION_A);
-	signal_init(proc, SIGCHLD, SIGACTION_I);
-	signal_init(proc, SIGCONT, SIGACTION_C);
-	signal_init(proc, SIGPOLL, SIGACTION_T);
-	signal_init(proc, SIGPROF, SIGACTION_T);
-	signal_init(proc, SIGSTOP, SIGACTION_S);
-	signal_init(proc, SIGSYS, SIGACTION_A);
-	signal_init(proc, SIGTSTP, SIGACTION_S);
-	signal_init(proc, SIGTTIN, SIGACTION_S);
-	signal_init(proc, SIGTTOU, SIGACTION_S);
-	signal_init(proc, SIGUSR1, SIGACTION_T);
-	signal_init(proc, SIGUSR2, SIGACTION_T);
-	signal_init(proc, SIGURG, SIGACTION_I);
-	signal_init(proc, SIGVTALRM, SIGACTION_T);
-	signal_init(proc, SIGXCPU, SIGACTION_A);
-	signal_init(proc, SIGXFSZ, SIGACTION_A);
-	signal_init(proc, SIGWINCH, SIGACTION_I);
-	signal_init(proc, SIGHUP, SIGACTION_T);
-	signal_init(proc, SIGINT, SIGACTION_T);
-	signal_init(proc, SIGQUIT, SIGACTION_A);
-	signal_init(proc, SIGILL, SIGACTION_A);
-	signal_init(proc, SIGTRAP, SIGACTION_A);
-	signal_init(proc, SIGABRT, SIGACTION_A);
-	signal_init(proc, SIGFPE, SIGACTION_A);
-	signal_init(proc, SIGKILL, SIGACTION_T);
-	signal_init(proc, SIGSEGV, SIGACTION_A);
-	signal_init(proc, SIGPIPE, SIGACTION_T);
-	signal_init(proc, SIGALRM, SIGACTION_T);
-	signal_init(proc, SIGTERM, SIGACTION_T);
+	signal_init(SIGBUS, SIGACTION_A);
+	signal_init(SIGCHLD, SIGACTION_I);
+	signal_init(SIGCONT, SIGACTION_C);
+	signal_init(SIGPOLL, SIGACTION_T);
+	signal_init(SIGPROF, SIGACTION_T);
+	signal_init(SIGSTOP, SIGACTION_S);
+	signal_init(SIGSYS, SIGACTION_A);
+	signal_init(SIGTSTP, SIGACTION_S);
+	signal_init(SIGTTIN, SIGACTION_S);
+	signal_init(SIGTTOU, SIGACTION_S);
+	signal_init(SIGUSR1, SIGACTION_T);
+	signal_init(SIGUSR2, SIGACTION_T);
+	signal_init(SIGURG, SIGACTION_I);
+	signal_init(SIGVTALRM, SIGACTION_T);
+	signal_init(SIGXCPU, SIGACTION_A);
+	signal_init(SIGXFSZ, SIGACTION_A);
+	signal_init(SIGWINCH, SIGACTION_I);
+	signal_init(SIGHUP, SIGACTION_T);
+	signal_init(SIGINT, SIGACTION_T);
+	signal_init(SIGQUIT, SIGACTION_A);
+	signal_init(SIGILL, SIGACTION_A);
+	signal_init(SIGTRAP, SIGACTION_A);
+	signal_init(SIGABRT, SIGACTION_A);
+	signal_init(SIGFPE, SIGACTION_A);
+	signal_init(SIGKILL, SIGACTION_T);
+	signal_init(SIGSEGV, SIGACTION_A);
+	signal_init(SIGPIPE, SIGACTION_T);
+	signal_init(SIGALRM, SIGACTION_T);
+	signal_init(SIGTERM, SIGACTION_T);
 }
