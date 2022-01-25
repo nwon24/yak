ab7d9b3d9021ebfcb8ace9ed614eee00b9be8840
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-25 14:31:09 +1100

    Added all signals required by POSIX

--- include/generic/signal.h
+++ include/generic/signal.h
@@ -1,29 +1,39 @@
 #ifndef SIGNAL_H
 #define SIGNAL_H
 
+#define NSIG		32
+
 #define SIGHUP		1
 #define SIGINT		2
 #define SIGQUIT		3
 #define SIGILL		4
 #define SIGTRAP		5
 #define SIGABRT		6
-#define SIGIOT		7
+#define SIGBUS		7
 #define SIGFPE		8
 #define SIGKILL		9
-#define SIGUSR1		10
+#define SIGCHLD		10
 #define SIGSEGV		11
-#define SIGUSR2		12
+#define SIGCONT		12
 #define SIGPIPE		13
 #define SIGALRM		14
 #define SIGTERM		15
-#define SIGSTKFLT	16
-#define SIGCHILD	17
-#define SIGCONT		18
-#define SIGSTOP		19
+#define SIGPOLL		16
+#define SIGPROF		17
+#define SIGSTOP		18
+#define SIGSYS		19
 #define SIGTSTP		20
 #define SIGTTIN		21
 #define SIGTTOU		22
+#define SIGUSR1		23
+#define SIGUSR2		24
+#define SIGURG		25
+#define SIGVTALRM	26
+#define SIGXCPU		27
+#define SIGXFSZ		28
+#define SIGWINCH	29
 
+/* 0 and 1 are not valid addresses of functions (hopefully) */
 #define SIG_DFL		((void (*)(int))0)
 #define SIG_IGN		((void (*)(int))1)
 
