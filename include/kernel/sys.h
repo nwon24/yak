1c470040a455994b2b8aa3a22a35e0d61c4478c3
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-26 21:30:07 +1100

    Register 'umask' and 'time' system calls

--- include/kernel/sys.h
+++ include/kernel/sys.h
@@ -17,6 +17,8 @@ gid_t kernel_getegid(void);
 int kernel_setuid(uid_t uid);
 int kernel_setgid(gid_t gid);
 pid_t kernel_setsid(void);
+mode_t kernel_umask(mode_t cmask);
+time_t kernel_time(time_t *tloc);
 
 static inline void
 sys_other_init(void)
@@ -28,6 +30,8 @@ sys_other_init(void)
 	register_syscall(__NR_setuid, (uint32_t)kernel_setuid, 1);
 	register_syscall(__NR_setgid, (uint32_t)kernel_setgid, 1);
 	register_syscall(__NR_setsid, (uint32_t)kernel_setsid, 0);
+	register_syscall(__NR_umask, (uint32_t)kernel_umask, 1);
+	register_syscall(__NR_time, (uint32_t)kernel_time, 1);
 }
 
 #endif /* SYS_H */
