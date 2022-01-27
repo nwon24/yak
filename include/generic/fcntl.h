82c7d074fb9dcb15a42e2eceeb49c45e3eaf208e
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-27 11:53:32 +1100

    Added a bunch of stuff

--- include/generic/fcntl.h
+++ include/generic/fcntl.h
@@ -18,5 +18,22 @@ int open(const char *path, int flags, ...);
 #define O_NOCTTY	00400
 #define O_TRUNC		01000
 #define O_APPEND	02000
+#define O_NONBLOCK	04000
+
+#define F_DUPFD		0
+#define F_DUPFD_CLOEXEC	1
+#define F_GETFD		2
+#define F_SETFD		3
+#define F_GETFL		4
+#define F_SETFL		5
+#define F_GETLK		6
+#define F_SETLK		7
+#define F_SETLKW	8
+#define F_SETOWN	9
+#define F_GETOWN	10
+#define F_SETSIG	11
+#define F_GETSIG	12
+
+#define FD_CLOEXEC	1
 
 #endif /* FCNTL_H */
