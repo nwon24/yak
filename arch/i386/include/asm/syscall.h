5fdb64c0e2b587aba2027aad1bc038dd18e29c85
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-27 13:24:29 +1100

    Use 'size_t' for syscall addr

--- arch/i386/include/asm/syscall.h
+++ arch/i386/include/asm/syscall.h
@@ -2,6 +2,7 @@
 #define SYSCALL_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 /* Should be enough for now */
 #define NR_SYSCALL 100
@@ -9,17 +10,17 @@
 #define SYSCALL_IRQ	0x80
 
 struct syscall_entry {
-	uint32_t addr;
+	size_t addr;
 	uint32_t nr_args;
 };
 
 extern struct syscall_entry syscall_table[];
 
 static inline void
-register_syscall(int n, uint32_t addr, uint32_t nr_args)
+register_syscall(int n, size_t addr, uint32_t nr_args)
 {
 	syscall_table[n].addr = addr;
 	syscall_table[n].nr_args = nr_args;
 }
- 
+
 #endif /* SYSCALL_H */
