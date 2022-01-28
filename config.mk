b2a081c01685b3db33597a68f367e4112c5ee946
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-29 09:37:01 +1100

    Use hosted toolchain (without -Werror option)

--- config.mk
+++ config.mk
@@ -1,12 +1,13 @@
+HOSTED_TARGET_TRIPLET := i686-hobbix
 TARGET_TRIPLET := i686-elf
 TARGET_ARCH := i386
 
 TOOLCHAIN ?= GNU
 
 ifeq ($(TOOLCHAIN), GNU)
-CC := $(TARGET_TRIPLET)-gcc
+CC := $(HOSTED_TARGET_TRIPLET)-gcc
 LD := $(CC)
-AR := $(TARGET_TRIPLET)-ar
+AR := $(HOSTED_TARGET_TRIPLET)-ar
 else ifeq ($(TOOLCHAIN), LLVM)
 CC := clang -target $(TARGET_TRIPLET)
 LD := ld.lld
@@ -16,15 +17,15 @@ $(error No toolchain specified. Use 'make TOOLCHAIN={GNU, LLVM}')
 endif
 CPP := $(CC) -E
 
-CFLAGS :=  -std=c99 -ffreestanding -O2 -Wall -Wextra -Werror -Winit-self -Wundef -pedantic -Wshadow -Wstrict-prototypes -MMD -MP -DKERNEL
+CFLAGS :=  -std=c99 -ffreestanding -O2 -Wall -Wextra -Winit-self -Wundef -pedantic -Wshadow -Wstrict-prototypes -MMD -MP -DKERNEL
 ifeq ($(CC), clang -target $(TARGET_TRIPLET))
 CFLAGS += -mno-sse
 endif
 CPPFLAGS := -ffreestanding -P
 # Still C compiler flags as we assemble .S files using GCC
 ASFLAGS := -ffreestanding -MMD -MP
-LDFLAGS := -nostdlib -O2 
-ifeq ($(CC), $(TARGET_TRIPLET)-gcc)
+LDFLAGS := -nostdlib -O2 -ffreestanding
+ifeq ($(CC), $(HOSTED_TARGET_TRIPLET)-gcc)
 LDFLAGS += -lgcc -Xlinker -Map=kernel.map
 else
 LDFLAGS += -Map kernel.map
