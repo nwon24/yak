bf570d921bc3de6814cc8cf16ed63141d9473963
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-28 17:57:46 +1100

    Added 'struct stat'

--- include/generic/stat.h
+++ include/generic/stat.h
@@ -37,4 +37,25 @@
 #define S_ISGID		002000
 #define S_ISUID		004000
 
+#ifndef __ASSEMBLER__
+
+#include <asm/types.h>
+
+struct stat {
+	dev_t st_dev;
+	ino_t st_ino;
+	mode_t st_mode;
+	nlink_t st_nlink;
+	uid_t st_uid;
+	gid_t st_gid;
+	dev_t st_rdev;
+	off_t st_size;
+	time_t st_atime;
+	time_t st_mtime;
+	time_t st_ctime;
+	blksize_t st_blksize;
+	blkcnt_t st_blocks;
+};
+
+#endif /* __ASSEMBLER__*/
 #endif /* STAT_H */
