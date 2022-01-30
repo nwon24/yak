37b361b1b364739b109b2697cf9912e83814cb8c
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-30 20:51:25 +1100

    Added some guards against library/assembly

--- sys/arch/i386/include/asm/syscall.h
+++ sys/arch/i386/include/asm/syscall.h
@@ -1,13 +1,17 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+#define SYSCALL_IRQ	0x80
+
+#ifndef __LIBRARY__
+
+#ifndef __ASSEMBLER__
+
 #include <stdint.h>
 #include <stddef.h>
 
 /* Should be enough for now */
 #define NR_SYSCALL 100
-
-#define SYSCALL_IRQ	0x80
 
 struct syscall_entry {
 	size_t addr;
@@ -22,5 +26,9 @@ register_syscall(int n, size_t addr, uint32_t nr_args)
 	syscall_table[n].addr = addr;
 	syscall_table[n].nr_args = nr_args;
 }
+
+#endif /* __ASSEMBLER__ */
+
+#endif /* __LIBRARY__ */
 
 #endif /* SYSCALL_H */
