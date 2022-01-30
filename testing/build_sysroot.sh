cca663101311dd7b696d40074d87f85c3b7ba68a
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-30 12:15:37 +1100

    Create '/dev/zero' and '/dev/null' when building sysroot

--- testing/build_sysroot.sh
+++ testing/build_sysroot.sh
@@ -4,7 +4,11 @@ mkdir -p $1/boot/grub/
 mkdir -p $1/dev
 sudo rm -f $1/dev/tty
 sudo rm -f $1/dev/tty0
+sudo rm -f $1/dev/null
+sudo rm -f $1/dev/zero
 sudo mknod $1/dev/tty c 4 0
 sudo mknod $1/dev/tty0 c 5 0
+sudo mknod $1/dev/null c 1 3
+sudo mknod $1/dev/zero c 1 5
 mkdir -p $1/usr/lib
 mkdir -p $1/usr/include
