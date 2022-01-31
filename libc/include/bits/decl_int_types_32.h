b9fe7465e2e6a3cb4caa297615b205925fc8eb18
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-31 16:15:08 +1100

    A lot of garbarge

--- libc/include/bits/decl_int_types_32.h
+++ libc/include/bits/decl_int_types_32.h
@@ -1,101 +1,118 @@
-#ifndef _BITS_DECL_INT_TYPES_32
-#define _BITS_DECL_INT_TYPES_32
-
-#if !defined(__DEFINED_PRIVATE_INT_TYPES)
-
-#ifdef __NEED_UINT8_T
+#if defined(__NEED_UINT8_T) && !defined(__DEFINED___UINT8_T)
 typedef unsigned char __uint8_t;
+#define __DEFINED___UINT8_T
 #endif
-#ifdef __NEED_UINT16_T
+#if defined(__NEED_UINT16_T) && !defined(__DEFINED___UINT16_T)
 typedef unsigned short __uint16_t;
+#define __DEFINED___UINT16_T
 #endif
-#ifdef __NEED_UINT32_T
+#if defined(__NEED_UINT32_T) && !defined(__DEFINED___UINT32_T)
 typedef unsigned long __uint32_t;
+#define __DEFINED___UINT32_T
 #endif
-#ifdef __NEED_UINT64_T
+#if defined(__NEED_UINT64_T) && !defined(__DEFINED___UINT64_T)
 typedef unsigned long long __uint64_t;
+#define __DEFINED___UINT64_T
 #endif
-#ifdef __NEED_INT8_T
+#if defined(__NEED_INT8_T) && !defined(__DEFINED___INT8_T)
 typedef signed char __int8_t;
+#define __DEFINED___INT8_T
 #endif
-#ifdef __NEED_INT16_T
+#if defined(__NEED_INT16_T) && !defined(__DEFINED___INT16_T)
 typedef short __int16_t;
+#define __DEFINED___INT16_T
 #endif
-#ifdef __NEED_INT32_T
+#if defined(__NEED_INT32_T) && !defined(__DEFINED___INT32_T)
 typedef long __int32_t;
+#define __DEFINED___INT32_T
 #endif
-#ifdef __NEED_INT64_T
+#if defined(__NEED_INT64_T) && !defined(__DEFINED___INT64_T)
 typedef long long __int64_t;
+#define __DEFINED___INT64_T
 #endif
 
-#ifdef __NEED_UINT_LEAST8_T
+#if defined(__NEED_UINT_LEAST8_T) && !defined(__DEFINED___UINT_LEAST8_T)
 typedef unsigned char __uint_least8_t;
+#define __DEFINED___UINT_LEAST8_T
 #endif
-#ifdef __NEED_UINT_LEAST16_T
+#if defined(__NEED_UINT_LEAST16_T) && !defined(__DEFINED___UINT_LEAST16_T)
 typedef unsigned short __uint_least16_t;
+#define __DEFINED___UINT_LEAST16_T
 #endif
-#ifdef __NEED_UINT_LEAST32_T
+#if defined(__NEED_UINT_LEAST32_T) && !defined(__DEFINED___UINT_LEAST32_T)
 typedef unsigned long __uint_least32_t;
+#define __DEFINED___UINT_LEAST32_T
 #endif
-#ifdef __NEED_UINT_LEAST64_T
+#if defined(__NEED_UINT_LEAST64_T) && !defined(__DEFINED___UINT_LEAST64_T)
 typedef unsigned long long __uint_least64_t;
+#define __DEFINED___UINT_LEAST64_T
 #endif
 
-#ifdef __NEED_INT_LEAST8_T
+#if defined(__NEED_INT_LEAST8_T) && !defined(__DEFINED___INT_LEAST8_T)
 typedef signed char __int_least8_t;
+#define __DEFINED___INT_LEAST8_T
 #endif
-#ifdef __NEED_INT_LEAST16_T
+#if defined(__NEED_INT_LEAST16_T) && !defined(__DEFINED___INT_LEAST16_T)
 typedef short __int_least16_t;
+#define __DEFINED___INT_LEAST16_T
 #endif
-#ifdef __NEED_INT_LEAST32_T
+#if defined(__NEED_INT_LEAST32_T) && !defined(__DEFINED___INT_LEAST32_T)
 typedef long __int_least32_t;
+#define __DEFINED___INT_LEAST32_T
 #endif
-#ifdef __NEED_INT_LEAST64_T
+#if defined(__NEED_INT_LEAST64_T) && !defined(__DEFINED___INT_LEAST64_T)
 typedef long long __int_least64_t;
+#define __DEFINED___INT_LEAST64_T
 #endif
 
-#ifdef __NEED_UINT_FAST8_T
+#if defined(__NEED_UINT_FAST8_T) && !defined(__DEFINED___UINT_FAST8_T)
 typedef unsigned char __uint_fast8_t;
+#define __DEFINED___UINT_FAST8_T
 #endif
-#ifdef __NEED_UINT_FAST16_T
+#if defined(__NEED_UINT_FAST16_T) && !defined(__DEFINED___UINT_FAST16_T)
 typedef unsigned short __uint_fast16_t;
+#define __DEFINED___UINT_FAST16_T
 #endif
-#ifdef __NEED_UINT_FAST32_T
+#if defined(__NEED_UINT_FAST32_T) && !defined(__DEFINED___UINT_FAST32_T)
 typedef unsigned long __uint_fast32_t;
+#define __DEFINED___UINT_FAST32_T
 #endif
-#ifdef __NEED_UINT_FAST64_T
+#if defined(__NEED_UINT_FAST64_T) && !defined(__DEFINED___UINT_FAST64_T)
 typedef unsigned long long __uint_fast64_t;
+#define __DEFINED___UINT_FAST64_T
 #endif
 
-#ifdef __NEED_INT_FAST8_T
+#if defined(__NEED_INT_FAST8_T) && !defined(__DEFINED___INT_FAST8_T)
 typedef signed char __int_fast8_t;
+#define __DEFINED___INT_FAST8_T
 #endif
-#ifdef __NEED_INT_FAST16_T
+#if defined(__NEED_INT_FAST16_T) && !defined(__DEFINED___INT_FAST16_T)
 typedef short __int_fast16_t;
+#define __DEFINED___INT_FAST16_T
 #endif
-#ifdef __NEED_INT_FAST32_T
+#if defined(__NEED_INT_FAST32_T) && !defined(__DEFINED___INT_FAST32_T)
 typedef long __int_fast32_t;
+#define __DEFINED___INT_FAST32_T
 #endif
-#ifdef __NEED_INT_FAST64_T
+#if defined(__NEED_INT_FAST64_T) && !defined(__DEFINED___INT_FAST64_T)
 typedef long long __int_fast64_t;
+#define __DEFINED___INT_FAST64_T
 #endif
 
-#ifdef __NEED_INTPTR_T
-typedef __int32_t __intptr_t;
+#if defined(__NEED_INTPTR_T) && !defined(__DEFINED___INTPTR_T)
+typedef long __intptr_t;
+#define __DEFINED___INTPTR_T
 #endif
-#ifdef __NEED_UINTPTR_T
-typedef __uint32_t __uintptr_t;
-#endif
-
-#ifdef __NEED_UINTMAX_T
-typedef __uint64_t __uintmax_t;
-#endif
-#ifdef __NEED_INTMAX_T
-typedef __int64_t __intmax_t;
+#if defined(__NEED_UINTPTR_T) && !defined(__DEFINED___UINTPTR_T)
+typedef unsigned long __uintptr_t;
+#define __DEFINED___UINTPTR_T
 #endif
 
-#define __DEFINED_PRIVATE_INT_TYPES
-
+#if defined(__NEED_UINTMAX_T) && !defined(__DEFINED___UINTMAX_T)
+typedef unsigned long __uintmax_t;
+#define __DEFINED___UINTMAX_T
 #endif
-
+#if defined(__NEED_INTMAX_T) && !defined(__DEFINED___INTMAX_T)
+typedef long long __intmax_t;
+#define __DEFINED___INTMAX_T
 #endif
