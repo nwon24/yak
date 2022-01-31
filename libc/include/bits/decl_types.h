e653a8d410277b73c612906508c03cb6b87b5cbf
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-31 15:12:17 +1100

    More preprocessor garbarge

--- libc/include/bits/decl_types.h
+++ libc/include/bits/decl_types.h
@@ -12,25 +12,49 @@
 #include <stddef.h>
 #define __SIZE_T_DEFINED
 
+#ifdef __NEED_PID_T
 typedef int __pid_t;
+#endif
+#ifdef __NEED_MODE_T
 typedef int __mode_t;
+#endif
+#ifdef __NEED_DEV_T
 typedef int __dev_t;
+#endif
+#ifdef __NEED_NLINK_T
 typedef int __nlink_t;
+#endif
+#ifdef __NEED_UID_T
 typedef int __uid_t;
+#endif
+#ifdef __NEED_ID_T
 typedef int __id_t;
+#endif
+#ifdef __NEED_GID_T
 typedef int __gid_t;
+#endif
+#ifdef __NEED_BLKCNT_T
 typedef signed int __blkcnt_t;
+#endif
+#ifdef __NEED_OFF_T
 typedef signed int __off_t;
+#endif
+#ifdef __NEED_FSBLKCNT_T
 typedef unsigned __fsblkcnt_t;
+#endif
+#ifdef __NEED_FSFILCNT_T
 typedef unsigned int __fsfilcnt_t;
+#endif
+#ifdef __NEED_INO_T
 typedef unsigned int __ino_t;
+#endif
 #if defined(__i386__)
-#if !defined(__SIZE_T_DEFINED)
+#if !defined(__SIZE_T_DEFINED) && defined(__NEED_SIZE_T)
 typedef __uint32_t __size_t;
 #endif
 typedef __int32_t __ssize_t;
 #elif defined(__x86_64__)
-#if !defined(__SIZE_T_DEFINED)
+#if !defined(__SIZE_T_DEFINED) && defined(__NEED_SSIZE_T)
 typedef __uint64_t __size_t;
 #endif
 typedef __int64_t __ssize_t;
