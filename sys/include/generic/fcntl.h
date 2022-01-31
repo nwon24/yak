9b011bac28d704444ca88a3d31f367c35d813e8f
nwon24 <WON0128@mhs.vic.edu.au>
2022-02-01 09:18:30 +1100

    Changed some things around

--- sys/include/generic/fcntl.h
+++ sys/include/generic/fcntl.h
@@ -1,18 +1,6 @@
 #ifndef FCNTL_H
 #define FCNTL_H
 
-#ifndef __LIBRARY__
-
-#ifndef __ASSEMBLER__
-
-#include <asm/types.h>
-
-int open(const char *path, int flags, ...);
-
-#endif /* __ASSEMBLER__ */
-
-#endif
-
 #define O_ACCMODE	03	/* Lower 2 bits of mode */
 #define O_RDONLY	00
 #define O_WRONLY	01
@@ -25,18 +13,19 @@ int open(const char *path, int flags, ...);
 #define O_NONBLOCK	04000
 
 #define F_DUPFD		0
-#define F_DUPFD_CLOEXEC	1
-#define F_GETFD		2
-#define F_SETFD		3
-#define F_GETFL		4
-#define F_SETFL		5
-#define F_GETLK		6
-#define F_SETLK		7
-#define F_SETLKW	8
-#define F_SETOWN	9
-#define F_GETOWN	10
-#define F_SETSIG	11
-#define F_GETSIG	12
+#define F_GETFD		1
+#define F_SETFD		2
+#define F_GETFL		3
+#define F_SETFL		4
+#define F_GETLK		5
+#define F_SETLK		6
+#define F_SETLKW	7
+#define F_SETOWN	8
+#define F_GETOWN	9
+#define F_SETSIG	10
+#define F_GETSIG	11
+
+#define F_DUPFD_CLOEXEC	1030
 
 #define FD_CLOEXEC	1
 
