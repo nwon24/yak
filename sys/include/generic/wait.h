9455430e64690aeba013351d1267eef62f1daca5
nwon24 <WON0128@mhs.vic.edu.au>
2022-01-31 19:20:53 +1100

    Rewrote kernel version of 'wait.h' to be consistent with library one

--- sys/include/generic/wait.h
+++ sys/include/generic/wait.h
@@ -1,8 +1,11 @@
 #ifndef WAIT_H
 #define WAIT_H
 
-#define WNOHANG		(1 << 0)
-#define WCONTINUED	(1 << 1)
-#define WUNTRACED	(1 << 2)
+#define WNOHANG         1
+#define WUNTRACED	2
+#define WSTOPPED	2
+#define WEXITED		4
+#define WCONTINUED	8
+#define WNOWAIT		0x1000000
 
 #endif /* WAIT_H */
