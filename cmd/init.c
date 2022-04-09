4d06293f4818a89cd65045199ebb4abe4e3d311b
nwon24 <WON0128@mhs.vic.edu.au>
2022-04-09 10:17:12 +1000

    Test canonical read

--- cmd/init.c
+++ cmd/init.c
@@ -2,6 +2,12 @@
 
 int main(int argc, char *argv[])
 {
+	int i;
+	char buf[512];
+
 	write(STDOUT_FILENO, "Hello, world!\n", 14);
+	while ((i = read(STDIN_FILENO, buf, sizeof(buf))) > 0)
+		write(STDOUT_FILENO, buf, i);
+	write(STDOUT_FILENO, "Done!\n", 6);
 	return 0;
 }
