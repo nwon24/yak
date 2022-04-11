9fc6b2f79c57f4f3870fce3da714a500a9965681
nwon24 <WON0128@mhs.vic.edu.au>
2022-04-11 13:04:33 +1000

    Version of init that tests 'execve' - current source of a bug
    Can't seem to 'execve' from here

--- cmd/init.c
+++ cmd/init.c
@@ -1,11 +1,17 @@
 #include <unistd.h>
 
+#include <sys/wait.h>
+
 int main(int argc, char *argv[])
 {
 	int i;
 	char buf[512];
+	const char *argv_new[] = { "/usr/bin/hello", NULL };
+	const char *envp_new[] = { "HOME=/usr/root", NULL };
 
-	write(STDOUT_FILENO, "Hello, world!\n", 14);
+	if (!fork()) {
+		execve("/usr/bin/hello", argv_new, envp_new);
+	}
 	while ((i = read(STDIN_FILENO, buf, sizeof(buf))) > 0)
 		write(STDOUT_FILENO, buf, i);
 	write(STDOUT_FILENO, "Done!\n", 6);
