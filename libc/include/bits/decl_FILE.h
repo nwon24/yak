dbfa6d254b6e5b5af102674f05515cb4a01a7848
nwon24 <WON0128@mhs.vic.edu.au>
2022-04-16 09:47:25 +1000

    Add a few more constants

--- libc/include/bits/decl_FILE.h
+++ libc/include/bits/decl_FILE.h
@@ -1,13 +1,17 @@
-#define __READ	1
-#define __WRITE	2
+#define __READ	(1 << 0)
+#define __WRITE	(1 << 1)
+#define __EOF	(1 << 2)
+#define __ERR	(1 << 3)
+
+#define __FILE_BUFSIZ	BUFSIZ
 
 struct __FILE {
 	int flags;
 	int fd;
 	int rcount;
 	int wcount;
-	char rbuf[BUFSIZ];
-	char wbuf[BUFSIZ];
+	char rbuf[__FILE_BUFSIZ];
+	char wbuf[__FILE_BUFSIZ];
 	char *rptr;
 	char *wptr;
 };
