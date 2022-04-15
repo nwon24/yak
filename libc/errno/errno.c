dba5a6a39ceca2037502dafa8e8c3361bf7b50c9
nwon24 <WON0128@mhs.vic.edu.au>
2022-04-16 08:57:18 +1000

    Implement errno

--- libc/errno/errno.c
+++ libc/errno/errno.c
@@ -1,1 +1,10 @@
 int errno;
+
+/*
+ * separate function for this in case we use threads later.
+ */
+void
+__set_errno(int val)
+{
+	errno = val;
+}
--- libc/include/errno.h
+++ libc/include/errno.h
@@ -1,4 +1,6 @@
 #ifndef _ERRNO_H
 #define _ERRNO_H
 
+#include <hobbix/generic/errno.h>
+
 #endif
