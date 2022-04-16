1bb2854e6e41fb95875964eda2db1f899bfdf043
nwon24 <WON0128@mhs.vic.edu.au>
2022-04-16 11:16:27 +1000

    Rename to bits/

--- libc/include/bits/stdio.h
+++ libc/include/bits/stdio.h
@@ -1,5 +1,5 @@
-#ifndef __STDIO_IMPL_H
-#define __STDIO_IMPL_H
+#ifndef __BITS_STDIO_H
+#define __BITS_STDIO_H
 
 int __fillbuf(FILE *fp);
 void __flushbuf(FILE *fp, int c);
@@ -7,12 +7,12 @@ void __flushbuf(FILE *fp, int c);
 void __libc_stdio_init(void);
 void __libc_stdio_fini(void);
 
-#define __getc_unlocked(fp)	__internal_getc(fp)
-#define __putc_unlocked(fp)	__internal_putc(fp)
+#define __getc_internal(fp)	__getc_unlocked(fp)
+#define __putc_internal(c, fp)	__putc_unlocked(c, fp)
 
-#define __internal_getc(fp) \
+#define __getc_unlocked(fp) \
 	(--((fp)->rcount) > 0 ? *(unsigned char *)((fp)->rptr++) : __fillbuf((fp)))
-#define __internal_putc(c, fp)			\
+#define __putc_unlocked(c, fp)			\
 	((fp)->wcount++ < __FILE_BUFSIZ ? *((fp)->wptr++) = (c) : __flushbuf((fp), (c)))
 
-#endif /* __STDIO_IMPL_H */
+#endif /* __BITS_STDIO_H */
