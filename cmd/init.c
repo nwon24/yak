de5c0dc9eb652478f0b93ef8d6528a3f92db1120
nwon24 <nathanw2047@gmail.com>
2022-04-21 10:39:05 +1000

    Smallest thing that works

--- cmd/init.c
+++ cmd/init.c
@@ -1,17 +1,13 @@
 #include <unistd.h>
 
 #include <sys/wait.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
 	int i;
 	char buf[512];
-	const char *argv_new[] = { "/usr/bin/hello", NULL };
-	const char *envp_new[] = { "HOME=/usr/root", NULL };
 
-	if (!fork()) {
-		execve("/usr/bin/hello", argv_new, envp_new);
-	}
 	while ((i = read(STDIN_FILENO, buf, sizeof(buf))) > 0)
 		write(STDOUT_FILENO, buf, i);
 	write(STDOUT_FILENO, "Done!\n", 6);
