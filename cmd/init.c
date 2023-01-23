fb0786816bb6880f1204c46748b1d1955e06ef63
nwon24 <nathanw2047@gmail.com>
2023-01-23 15:06:27 +1100

    Get path of hello right

--- cmd/init.c
+++ cmd/init.c
@@ -3,14 +3,26 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
+const char *hello = "/usr/bin/hello";
+const char *hello_argv[] = { NULL };
+const char *hello_envp[] = { NULL };
+
 int main(void)
 {
 	int i;
 	char buf[512];
 
-/*	write(1, "Hello\n", 6); */
+	write(1, "Hello\n", 6);
 	while ((i = read(STDIN_FILENO, buf, sizeof(buf))) > 0)
 		write(STDOUT_FILENO, buf, i);
+
+	if (!fork()) {
+		execve(hello, hello_argv, hello_envp);	
+		write(1, "here?\n", 6);
+	} else {
+		wait(&i);
+		write(1, "blah\n", 5);
+	}
 	write(STDOUT_FILENO, "Done!\n", 6); 
 	return 0;
 }
