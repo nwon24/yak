e24a1a336e8b2a11afa1eaa90c7201eed56b3902
nwon24 <nathanw2047@gmail.com>
2023-01-23 15:08:15 +1100

    Finally fixed COW bug!!!!!!!

--- sys/arch/i386/virtual_mm.c
+++ sys/arch/i386/virtual_mm.c
@@ -30,7 +30,7 @@ static int virt_map_entry = 1023;
 static void free_page_table(uint32_t page_table);
 static void pg_table_cow(uint32_t pg_table, int inc_ref);
 
-static void page_set_writable(uintptr_t addr, int copy);
+static void page_set_writable(uintptr_t addr);
 
 static inline void
 tmp_map_page(uint32_t page)
@@ -249,8 +249,10 @@ copy_address_space(uint32_t from_page_dir, uint32_t to_page_dir)
 	 * Only need to increase reference count once, so we don't do it on the first 'page_table_cow'
 	 */
 	for (p = (uint32_t *)VIRT_ADDR_TMP_PAGE; p < (uint32_t *)VIRT_ADDR_TMP_PAGE + PAGE_SIZE / sizeof(uint32_t); p++) {
-		if ((*p & PAGE_PRESENT) && (*p & PAGE_USER))
+		if ((*p & PAGE_PRESENT) && (*p & PAGE_USER)) {
 			pg_table_cow(*p, 0);
+			*p &= ~PAGE_WRITABLE;
+		}
 	}
 	tmp_map_page(from_page_dir);
 	tlb_flush(VIRT_ADDR_TMP_PAGE);
@@ -271,6 +273,7 @@ copy_address_space(uint32_t from_page_dir, uint32_t to_page_dir)
 out:
 	tmp_map_page(old);
 	tlb_flush(VIRT_ADDR_TMP_PAGE);
+	reload_cr3();
 }
 
 static void
@@ -472,15 +475,11 @@ handle_page_fault(uint32_t error)
 			kernel_exit(SIGSEGV);
 		}
 	}
-	if (page_get_count(addr & 0xFFFFF000) > 1) {
-		page_set_writable(addr, 1);
-	} else {
-		page_set_writable(addr, 0);
-	}
+	page_set_writable(addr);
 }
 
 static void
-page_set_writable(uintptr_t addr, int copy)
+page_set_writable(uintptr_t addr)
 {
 	uint32_t old, new, *ptr, pg_frame, pg_table;
 	char tmp_page[PAGE_SIZE];
@@ -508,24 +507,26 @@ page_set_writable(uintptr_t addr, int copy)
 		printk("Segmentation fault\r\n");
 		kernel_exit(SIGSEGV);
 	}
-	if (copy) {
+	if (page_get_count(pg_frame & 0xFFFFF000) > 1) {
 		new = page_frame_alloc();
 		if (new == NO_FREE_PAGE)
 			panic("Out of memory");
-		page_frame_free(*ptr & 0xFFFFF000);
 		*ptr = new | old_flags | PAGE_WRITABLE;
-		page_decrease_count(addr & 0xFFFFF000);
+		page_decrease_count(pg_frame & 0xFFFFF000);
 		tmp_map_page(pg_frame & 0xFFFFF000);
 		tlb_flush(VIRT_ADDR_TMP_PAGE);
 		memmove(tmp_page, (void *)VIRT_ADDR_TMP_PAGE, PAGE_SIZE);
 		tmp_map_page(new);
 		tlb_flush(VIRT_ADDR_TMP_PAGE);
 		memmove((void *)VIRT_ADDR_TMP_PAGE, tmp_page, PAGE_SIZE);
+		if (page_get_count(pg_frame & 0xFFFFF000) == 0)
+			page_frame_free(pg_frame & 0xFFFFF000);
 	} else {
 		*ptr |= PAGE_WRITABLE;
 	}
 	tlb_flush(addr);
 	tmp_map_page(old);
 	tlb_flush(VIRT_ADDR_TMP_PAGE);
+	reload_cr3();
 }
 
